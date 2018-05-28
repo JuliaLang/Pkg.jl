6d8a43be4d2b4871e233b25ea1da1bebf848fd1c
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2018-05-28 13:45:35 +0200

    only update the registry on add once per session (#307)

--- src/Pkg.jl
+++ src/Pkg.jl
@@ -8,6 +8,7 @@ using REPL.TerminalMenus
 depots() = Base.DEPOT_PATH
 logdir() = joinpath(depots()[1], "logs")
 devdir() = get(ENV, "JULIA_PKG_DEVDIR", joinpath(homedir(), ".julia", "dev"))
+const UPDATED_REGISTRY_THIS_SESSION = Ref(false)
 
 have_warned_session = false
 function print_first_command_header()
