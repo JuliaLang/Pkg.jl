271139ed8bfa1c386700c8d46db423454e7de711
Kristoffer Carlsson <kcarlsson89@gmail.com>
2018-06-13 15:55:11 +0200

    fix casesensitiveness on adding packages

--- src/REPLMode.jl
+++ src/REPLMode.jl
@@ -6,7 +6,7 @@ using UUIDs
 import REPL
 import REPL: LineEdit, REPLCompletions
 
-import ..devdir, ..print_first_command_header, ..Types.isdir_windows_workaround
+import ..devdir, ..print_first_command_header, ..Types.casesensitive_isdir
 using ..Types, ..Display, ..Operations, ..API
 
 ############
@@ -140,7 +140,7 @@ end
 
 function parse_package(word::AbstractString; context=nothing)::PackageSpec
     word = replace(word, "~" => homedir())
-    if context in (CMD_ADD, CMD_DEVELOP) && isdir_windows_workaround(word)
+    if context in (CMD_ADD, CMD_DEVELOP) && casesensitive_isdir(word)
         pkg = PackageSpec()
         pkg.repo = Types.GitRepo(abspath(word))
         return pkg
