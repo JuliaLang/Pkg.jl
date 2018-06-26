bc01765e785b4db4682f3648ad2db6d840b337f4
Fredrik Ekre <fredrik.ekre@chalmers.se>
2018-06-26 15:44:04 +0200

    fold deactivate into no-arg activate

--- src/Pkg.jl
+++ src/Pkg.jl
@@ -58,7 +58,6 @@ const resolve     = API.resolve
 const status      = Display.status
 const update      = up
 const activate    = API.activate
-const deactivate  = API.deactivate
 
 # legacy CI script support
 import .API: clone, dir
