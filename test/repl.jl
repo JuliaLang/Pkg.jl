afccd5b4dc19cbc74b7c9cd0f0f8c418377e218d
David Varela <00.varela.david@gmail.com>
2018-07-09 18:18:05 -0700

    Remove debug code

--- test/repl.jl
+++ test/repl.jl
@@ -22,47 +22,6 @@ function git_init_package(tmp, path)
     return pkgpath
 end
 
-@testset "isolating error" begin
-    function set_name(projfile_path, newname)
-        sleep(1.1)
-        project = Pkg.TOML.parsefile(projfile_path)
-        project["name"] = newname
-        open(projfile_path, "w") do io
-            Pkg.TOML.print(io, project)
-        end
-    end
-
-    with_temp_env("SomeEnv") do
-        @test Pkg.REPLMode.promptf() == "(SomeEnv) pkg> "
-    end
-
-    env_name = "Test2"
-    with_temp_env(env_name) do env_path
-        projfile_path = joinpath(env_path, "Project.toml")
-        @test Pkg.REPLMode.promptf() == "($env_name) pkg> "
-
-        newname = "NewName"
-        set_name(projfile_path, newname)
-        @test Pkg.REPLMode.promptf() == "($env_name) pkg> "
-        cd(env_path) do
-            @test Pkg.REPLMode.promptf() == "($env_name) pkg> "
-        end
-        @test Pkg.REPLMode.promptf() == "($env_name) pkg> "
-
-        println("----") #debug
-        newname = "NewNameII"
-        set_name(projfile_path, newname)
-        cd(env_path) do
-            @test Pkg.REPLMode.promptf() == "($newname) pkg> "
-        end
-        @test Pkg.REPLMode.promptf() == "($newname) pkg> "
-
-        rm(projfile_path)
-        @test Pkg.REPLMode.promptf() == "($env_name) pkg> "
-    end
-end
-
-#=
 @testset "generate args" begin
     @test_throws CommandError pkg"generate"
 end
@@ -495,10 +454,9 @@ end
         end
         @test Pkg.REPLMode.promptf() == "($newname) pkg> "
 
-        rm(projfile_path)
-        @test Pkg.REPLMode.promptf() == "($env_name) pkg> "
+        #rm(projfile_path)
+        #@test Pkg.REPLMode.promptf() == "($env_name) pkg> "
     end
 end
-=#
 
 end # module
