b822b5be817744cc685b57d1e754a2c4fc10a7a3
David Varela <00.varela.david@gmail.com>
2018-07-20 22:46:38 -0700

    Remove (accidentally added) debug code

--- src/REPLMode.jl
+++ src/REPLMode.jl
@@ -360,8 +360,6 @@ function enforce_command_spec(spec::CommandSpec, statement::Statement)
     # argument count
     if !isempty(spec.arg_spec)
         arg_count = length(statement.arguments)
-        println("** arg_count: [$arg_count]")
-        println("** spec: [$(spec.arg_spec)]")
         #TODO better error messages: length = 0 say no; length > 1 say 'or' ...
         arg_count in spec.arg_spec ||
             cmderror("Command `$(statement.command)` expects $(spec.arg_spec) arguments, but given $arg_count.")
