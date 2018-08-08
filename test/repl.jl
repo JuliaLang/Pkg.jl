33d16873a8453cbdca62ebb134ea2ca709469c56
David Varela <00.varela.david@gmail.com>
2018-08-08 16:39:19 -0700

    Update tests for `preview`

--- test/repl.jl
+++ test/repl.jl
@@ -873,10 +873,10 @@ end
 @testset "preview" begin
     temp_pkg_dir() do project_path; cd_tempdir() do tmpdir; with_temp_env() do;
         pkg"add Example"
-        pkg"preview 'rm Example'"
+        pkg"--preview rm Example"
         @test isinstalled(TEST_PKG)
         pkg"rm Example"
-        pkg"preview 'add Example'"
+        pkg"--preview add Example"
         @test !isinstalled(TEST_PKG)
     end end end
 end
