cae8b4fa608a23e02673ff3b9f5608c064889640
Fredrik Ekre <ekrefredrik@gmail.com>
2019-02-20 13:17:01 +0100

    Overload Base.showerror instead of Base.show for PkgError.

--- src/Types.jl
+++ src/Types.jl
@@ -110,7 +110,7 @@ struct PkgError <: Exception
     msg::String
 end
 pkgerror(msg::String...) = throw(PkgError(join(msg)))
-Base.show(io::IO, err::PkgError) = print(io, err.msg)
+Base.showerror(io::IO, err::PkgError) = print(io, err.msg)
 
 
 ###############
