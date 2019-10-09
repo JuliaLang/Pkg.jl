fc9151767b261ac1529e67ea57578c27ea280625
Elliot Saba <staticfloat@gmail.com>
2019-10-09 10:59:31 -0700

    Change direct path to `7z` to point toward `libexec`, not `bin`

--- src/PlatformEngines.jl
+++ src/PlatformEngines.jl
@@ -384,8 +384,8 @@ function probe_platform_engines!(;verbose::Bool = false)
         # We greatly prefer `7z` as a compression engine on Windows
         prepend!(compression_engines, [(`7z --help`, gen_7z("7z")...)])
 
-        # On windows, we bundle 7z with Julia, so try invoking that directly
-        exe7z = joinpath(Sys.BINDIR, "7z.exe")
+        # We bundle 7z with Julia, so try invoking that directly as well
+        exe7z = joinpath(Sys.BINDIR, "..", "libexec", "7z.exe")
         prepend!(compression_engines, [(`$exe7z --help`, gen_7z(exe7z)...)])
     end
 
