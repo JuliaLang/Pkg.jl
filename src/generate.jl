eae1b9ea35be23f715ec435a610d6323a0bed133
Dilum Aluthge <dilum@aluthge.com>
2019-11-10 00:57:50 -0500

    Pkg.generate should set `1.0.0` as the initial version number of a package

--- src/generate.jl
+++ src/generate.jl
@@ -50,7 +50,7 @@ function project(ctx::Context, pkg::String, dir::String)
         toml = Dict("authors" => authors,
                     "name" => pkg,
                     "uuid" => string(uuid),
-                    "version" => "0.1.0",
+                    "version" => "1.0.0",
                     )
         TOML.print(io, toml, sorted=true, by=key -> (Types.project_key_order(key), key))
     end
