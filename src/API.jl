68c2ee108197922a9e8d2233e5e269c594dcb8b9
Steve Kelly <kd2cca@gmail.com>
2019-12-24 05:25:20 -0500

    Update doc string for #1540 (#1562)

--- src/API.jl
+++ src/API.jl
@@ -315,13 +315,13 @@ function test(ctx::Context, pkgs::Vector{PackageSpec};
 end
 
 """
-    gc(ctx::Context=Context(); collect_delay::Period=Day(30), kwargs...)
+    gc(ctx::Context=Context(); collect_delay::Period=Day(7), kwargs...)
 
 Garbage-collect package and artifact installations by sweeping over all known
 `Manifest.toml` and `Artifacts.toml` files, noting those that have been deleted, and then
 finding artifacts and packages that are thereafter not used by any other projects.  This
 method will only remove package versions and artifacts that have been continually un-used
-for a period of `collect_delay`; which defaults to thirty days.
+for a period of `collect_delay`; which defaults to seven days.
 """
 function gc(ctx::Context=Context(); collect_delay::Period=Day(7), kwargs...)
     Context!(ctx; kwargs...)
