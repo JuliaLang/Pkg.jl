JuliaLang/Pkg.jl

commit 44e8977758c625c04400af7995cf79acd6a32b3c

Rework repo handling (#1372)

src/REPLMode/argument_parsers.jl
@@ -73,11 +73,11 @@ end
 # packages can be identified through: uuid, name, or name+uuid
 # additionally valid for add/develop are: local path, url
 function parse_package_identifier(word::AbstractString; add_or_develop=false)::PackageSpec
-    if add_or_develop && casesensitive_isdir(expanduser(word))
+    if add_or_develop && isdir_windows_workaround(expanduser(word))
         if !occursin(Base.Filesystem.path_separator_re, word)
             @info "Resolving package identifier `$word` as a directory at `$(Base.contractuser(abspath(word)))`."
         end
-        return PackageSpec(repo=Types.GitRepo(url=expanduser(word)))
+        return PackageSpec(repo=Types.GitRepo(source=expanduser(word)))
     elseif occursin(uuid_re, word)
         return PackageSpec(uuid=UUID(word))
     elseif occursin(name_re, word)
@@ -85,10 +85,8 @@ function parse_package_identifier(word::AbstractString; add_or_develop=false)::P
     elseif occursin(name_uuid_re, word)
         m = match(name_uuid_re, word)
         return PackageSpec(String(m.captures[1]), UUID(m.captures[2]))
-    elseif add_or_develop
-        @info "Resolving package identifier `$word` as a URL."
-        # Guess it is a url then
-        return PackageSpec(repo=Types.GitRepo(url=word))
+    elseif add_or_develop && isurl(word)
+        return PackageSpec(repo=Types.GitRepo(source=word))
     else
         pkgerror("Unable to parse `$word` as a package.")
     end

src/API.jl
@@ -37,8 +37,8 @@ function project(ctx::Context)
 end
 
 function check_package_name(x::AbstractString, mode=nothing)
-    if !(occursin(Pkg.REPLMode.name_re, x))
-        message = "$x is not a valid packagename."
+    if !Base.isidentifier(x)
+        message = "`$x` is not a valid package name."
         if mode !== nothing && any(occursin.(['\\','/'], x)) # maybe a url or a path
             message *= "\nThe argument appears to be a URL or path, perhaps you meant " *
                 "`Pkg.$mode(PackageSpec(url=\"...\"))` or `Pkg.$mode(PackageSpec(path=\"...\"))`."
@@ -46,14 +46,6 @@ function check_package_name(x::AbstractString, mode=nothing)
         pkgerror(message)
     end
     return PackageSpec(x)
-end
-
-function err_rep(pkg::PackageSpec)
-    x = pkg.name !== nothing && pkg.uuid !== nothing ? x = "$name [$(string(pkg.uuid)[1:8])]" :
-        pkg.name !== nothing ? pkg.name :
-        pkg.uuid !== nothing ? string(pkg.uuid)[1:8] :
-        pkg.repo.url
-    return "`$x`"
 end
 
 develop(pkg::Union{AbstractString, PackageSpec}; kwargs...) = develop([pkg]; kwargs...)
@@ -70,7 +62,7 @@ function develop(ctx::Context, pkgs::Vector{PackageSpec}; shared::Bool=true,
         if pkg.name == "julia"
             pkgerror("`julia` is not a valid package name.")
         end
-        if pkg.name === nothing && pkg.uuid === nothing && pkg.repo.url === nothing
+        if pkg.name === nothing && pkg.uuid === nothing && pkg.repo.source === nothing
             pkgerror("When calling `develop`, packages must be specified by name, UUID, URL, or filesystem path.")
         end
         if pkg.repo.rev !== nothing
@@ -106,21 +98,23 @@ function add(ctx::Context, pkgs::Vector{PackageSpec}; preserve::PreserveLevel=PR
         if pkg.name == "julia"
             pkgerror("`julia` is not a valid package name.")
         end
-        if pkg.name === nothing && pkg.uuid === nothing && pkg.repo.url === nothing
+        if pkg.name === nothing && pkg.uuid === nothing && pkg.repo.source === nothing
             pkgerror("When calling `add`, packages must be specified by name, UUID, URL, or filesystem path.")
         end
-        if (pkg.repo.url !== nothing || pkg.repo.rev !== nothing)
-            pkg.version == VersionSpec() ||
-                pkgerror("Can not specify version when tracking a repo.")
+        if pkg.repo.source !== nothing || pkg.repo.rev !== nothing
+            if pkg.version != VersionSpec()
+                pkgerror("It is invalid to specify a version when tracking a git repository:",
+                         " `$(pkg.version)` given for package $(err_rep(pkg)).")
+            end
         end
     end
 
-    Types.update_registries(ctx)
-
-    repo_pkgs = [pkg for pkg in pkgs if (pkg.repo.url !== nothing || pkg.repo.rev !== nothing)]
+    repo_pkgs = [pkg for pkg in pkgs if (pkg.repo.source !== nothing || pkg.repo.rev !== nothing)]
     new_git = handle_repos_add!(ctx, repo_pkgs)
-    # repo + unpinned -> name, uuid, repo.rev, repo.url, tree_hash
+    # repo + unpinned -> name, uuid, repo.rev, repo.source, tree_hash
     # repo + pinned -> name, uuid, tree_hash
+
+    Types.update_registries(ctx)
 
     project_deps_resolve!(ctx, pkgs)
     registry_resolve!(ctx, pkgs)
@@ -147,7 +141,7 @@ function rm(ctx::Context, pkgs::Vector{PackageSpec}; mode=PKGMODE_PROJECT, kwarg
             pkgerror("When calling `rm`, packages must be specified by name or UUID.")
         end
         if !(pkg.version == VersionSpec() && pkg.pinned == false &&
-             pkg.tree_hash === nothing && pkg.repo.url === nothing &&
+             pkg.tree_hash === nothing && pkg.repo.source === nothing &&
              pkg.repo.rev === nothing && pkg.path === nothing)
             pkgerror("When calling `rm`, packages may only be specified by name or UUID.")
         end
@@ -213,9 +207,9 @@ function pin(ctx::Context, pkgs::Vector{PackageSpec}; kwargs...)
         if pkg.name === nothing && pkg.uuid === nothing
             pkgerror("When calling `pin`, packages must be specified by name or UUID.")
         end
-        if pkg.repo.url !== nothing
+        if pkg.repo.source !== nothing
             pkgerror("It is invalid to specify a repo location when calling `pin`:",
-                     " `$(pkg.repo.url)` given for package $(err_rep(pkg)).")
+                     " `$(pkg.repo.source)` given for package $(err_rep(pkg)).")
         end
         if pkg.repo.rev !== nothing
             pkgerror("It is invalid to specify a git revision when calling `pin`:",
@@ -244,7 +238,7 @@ function free(ctx::Context, pkgs::Vector{PackageSpec}; kwargs...)
             pkgerror("When calling `free`, packages must be specified by name or UUID.")
         end
         if !(pkg.version == VersionSpec() && pkg.pinned == false &&
-             pkg.tree_hash === nothing && pkg.repo.url === nothing &&
+             pkg.tree_hash === nothing && pkg.repo.source === nothing &&
              pkg.repo.rev === nothing && pkg.path === nothing)
             pkgerror("When calling `free`, packages may only be specified by name or UUID.")
         end
@@ -701,6 +695,19 @@ function precompile(ctx::Context)
     nothing
 end
 
+function tree_hash_exists(repo_path::String, tree_hash::String)
+    isdir(repo_path) || return false
+    LibGit2.with(LibGit2.GitRepo(repo_path)) do repo
+        try
+            LibGit2.GitObject(repo, tree_hash)
+            return true
+        catch err
+            err isa LibGit2.GitError && err.code == LibGit2.Error.ENOTFOUND || rethrow()
+        end
+        return false
+    end
+end
+
 instantiate(; kwargs...) = instantiate(Context(); kwargs...)
 function instantiate(ctx::Context; manifest::Union{Bool, Nothing}=nothing,
                      update_registry::Bool=true, verbose::Bool=false,
@@ -719,7 +726,7 @@ function instantiate(ctx::Context; manifest::Union{Bool, Nothing}=nothing,
         Types.write_project(Dict("deps" => deps), ctx.env.project_file)
         return instantiate(Context(); manifest=manifest, update_registry=update_registry, verbose=verbose, kwargs...)
     end
-    if (!isfile(ctx.env.manifest_file) && manifest == nothing) || manifest == false
+    if (!isfile(ctx.env.manifest_file) && manifest === nothing) || manifest == false
         up(ctx; update_registry=update_registry)
         return
     end
@@ -742,16 +749,33 @@ function instantiate(ctx::Context; manifest::Union{Bool, Nothing}=nothing,
     pkgs = Operations.load_all_deps(ctx)
     Operations.check_registered(ctx, pkgs)
     new_git = UUID[]
+    # Handling packages tracking repos
     for pkg in pkgs
-        pkg.repo.url !== nothing || continue
+        pkg.repo.source !== nothing || continue
         sourcepath = Operations.source_path(pkg)
         isdir(sourcepath) && continue
-        # download repo at tree hash
-        push!(new_git, pkg.uuid)
-        clonepath = Types.clone_path!(ctx, pkg.repo.url)
-        tmp_source = Types.repo_checkout(ctx, clonepath, string(pkg.tree_hash))
+        ## Download repo at tree hash
+        # determine canonical form of repo source
+        if isurl(pkg.repo.source)
+            repo_source = pkg.repo.source
+        else
+            repo_source = normpath(joinpath(dirname(ctx.env.project_file), pkg.repo.source))
+        end
+        clonepath = Types.clone_path(repo_source)
+        # We try to avoid updating the clone if the target already exists
+        if tree_hash_exists(clonepath, string(pkg.tree_hash))
+            tmp_source = Types.repo_checkout(ctx, clonepath, string(pkg.tree_hash))
+        else
+            if !isurl(repo_source) && !isdir(repo_source)
+                pkgerror("Expected $(err_rep(pkg)) to exist at `$(repo_source)`, but no such directory.")
+            end
+            Types.clone_path!(ctx, repo_source) # TODO a more direct way of updating a clone
+            tmp_source = Types.repo_checkout(ctx, clonepath, string(pkg.tree_hash))
+        end
+        # Finally move the checked out tree to the correct location
         mkpath(sourcepath)
         mv(tmp_source, sourcepath; force=true)
+        push!(new_git, pkg.uuid)
     end
 
     # Ensure artifacts are installed for the dependent packages, and finally this overall project
@@ -872,13 +896,13 @@ function Package(;name::Union{Nothing,AbstractString} = nothing,
                  version::Union{VersionNumber, String, VersionSpec, Nothing} = nothing,
                  url = nothing, rev = nothing, path=nothing, mode::PackageMode = PKGMODE_PROJECT)
     if path !== nothing && url !== nothing
-        pkgerror("It is invalid to specify both `path` and `url`.")
+        pkgerror("It is invalid to specify both a path and url.")
     end
     if url !== nothing && version !== nothing
         pkgerror("It is invalid to specify both `version` and `url`.",
                  "Hint: `rev` may have been intended instead of `version.")
     end
-    repo = Types.GitRepo(rev = rev, url = url !== nothing ? url : path)
+    repo = Types.GitRepo(rev = rev, source = url !== nothing ? url : path)
     version = version === nothing ? VersionSpec() : VersionSpec(version)
     uuid isa String && (uuid = UUID(uuid))
     PackageSpec(;name=name, uuid=uuid, version=version, mode=mode, path=nothing,

src/Display.jl
@@ -156,7 +156,7 @@ vstring(ctx::Context, a::VerInfo) =
     string((a.ver == nothing && a.hash != nothing) ? "[$(string(a.hash)[1:16])]" : "",
            a.ver != nothing ? "v$(a.ver)" : "",
            a.path != nothing ? " [$(pathrepr(a.path))]" : "",
-           a.repo != nothing ? " #$(revstring(a.repo.rev)) ($(a.repo.url))" : "",
+           a.repo != nothing ? " #$(revstring(a.repo.rev)) ($(a.repo.source))" : "",
            a.pinned == true ? " ⚲" : "",
            )
 
@@ -246,7 +246,7 @@ function name_ver_info(entry::PackageEntry)
         entry.path,
         entry.version,
         entry.pinned,
-        entry.repo.url === nothing ? nothing : entry.repo, # TODO
+        entry.repo.source === nothing ? nothing : entry.repo, # TODO
         )
 end
 

src/GitTools.jl
@@ -103,8 +103,13 @@ function normalize_url(url::AbstractString)
     end
 end
 
-ensure_clone(ctx, target_path, url; kwargs...) =
-    ispath(target_path) ? LibGit2.GitRepo(target_path) : GitTools.clone(ctx, url, target_path; kwargs...)
+function ensure_clone(ctx, target_path, url; kwargs...)
+    if ispath(target_path)
+        return LibGit2.GitRepo(target_path)
+    else
+        return GitTools.clone(ctx, url, target_path; kwargs...)
+    end
+end
 
 function clone(ctx, url, source_path; header=nothing, kwargs...)
     @assert !isdir(source_path) || isempty(readdir(source_path))

src/Operations.jl
@@ -33,13 +33,13 @@ end
 # more accurate name is `should_be_tracking_registered_version`
 # the only way to know for sure is to key into the registries
 tracking_registered_version(pkg) =
-    !is_stdlib(pkg.uuid) && pkg.path === nothing && pkg.repo.url === nothing
+    !is_stdlib(pkg.uuid) && pkg.path === nothing && pkg.repo.source === nothing
 
 function source_path(pkg::PackageSpec)
     return is_stdlib(pkg.uuid)    ? Types.stdlib_path(pkg.name) :
-        pkg.path      !== nothing ? pkg.path :
-        pkg.repo.url  !== nothing ? find_installed(pkg.name, pkg.uuid, pkg.tree_hash) :
-        pkg.tree_hash !== nothing ? find_installed(pkg.name, pkg.uuid, pkg.tree_hash) :
+        pkg.path        !== nothing ? pkg.path :
+        pkg.repo.source !== nothing ? find_installed(pkg.name, pkg.uuid, pkg.tree_hash) :
+        pkg.tree_hash   !== nothing ? find_installed(pkg.name, pkg.uuid, pkg.tree_hash) :
         nothing
 end
 
@@ -243,7 +243,7 @@ function collect_project!(ctx::Context, pkg::PackageSpec, path::String, fix_deps
     return
 end
 
-istracking(pkg) = pkg.path !== nothing || pkg.repo.url !== nothing
+istracking(pkg) = pkg.path !== nothing || pkg.repo.source !== nothing
 isfixed(pkg) = istracking(pkg) || pkg.pinned
 
 function collect_fixed!(ctx::Context, pkgs::Vector{PackageSpec}, names::Dict{UUID, String})
@@ -940,7 +940,7 @@ function update_package_add(pkg::PackageSpec, entry::PackageEntry, is_dep::Bool)
         return PackageSpec(; uuid=pkg.uuid, name=pkg.name, pinned=true,
                            version=entry.version, tree_hash=entry.tree_hash)
     end
-    if entry.path !== nothing || entry.repo.url !== nothing || pkg.repo.url !== nothing
+    if entry.path !== nothing || entry.repo.source !== nothing || pkg.repo.source !== nothing
         return pkg # overwrite everything, nothing to copy over
     end
     if is_stdlib(pkg.uuid)
@@ -1078,7 +1078,10 @@ end
 up_load_versions!(ctx::Context, pkg::PackageSpec, ::Nothing, level::UpgradeLevel) = false
 function up_load_versions!(ctx::Context, pkg::PackageSpec, entry::PackageEntry, level::UpgradeLevel)
     entry.version !== nothing || return false # no version to set
-    if entry.repo.url !== nothing # repo packages have a version but are treated special
+    if entry.pinned || level == UPLEVEL_FIXED
+        pkg.version = entry.version
+        pkg.tree_hash = entry.tree_hash
+    elseif entry.repo.source !== nothing # repo packages have a version but are treated special
         pkg.repo = entry.repo
         if level == UPLEVEL_MAJOR
             new = instantiate_pkg_repo!(ctx, pkg)
@@ -1091,9 +1094,6 @@ function up_load_versions!(ctx::Context, pkg::PackageSpec, entry::PackageEntry, 
             pkg.version = entry.version
             pkg.tree_hash = entry.tree_hash
         end
-    elseif entry.pinned || level == UPLEVEL_FIXED
-        pkg.version = entry.version
-        pkg.tree_hash = entry.tree_hash
     else
         ver = entry.version
         r = level == UPLEVEL_PATCH ? VersionRange(ver.major, ver.minor) :
@@ -1165,7 +1165,7 @@ function update_package_pin!(ctx::Context, pkg::PackageSpec, entry::PackageEntry
         pkg.tree_hash = entry.tree_hash
         pkg.path = entry.path
     else # given explicit registered version
-        if entry.repo.url !== nothing || entry.path !== nothing
+        if entry.repo.source !== nothing || entry.path !== nothing
             # A pin in this case includes an implicit `free` to switch to tracking registered versions
             # First, make sure the package is registered so we have something to free to
             if isempty(registered_paths(ctx, pkg.uuid))
@@ -1204,7 +1204,7 @@ function update_package_free!(ctx::Context, pkg::PackageSpec, entry::PackageEntr
     if entry.path !== nothing # deved
         return # -> name, uuid
     end
-    if entry.repo !== nothing # tracking a repo
+    if entry.repo.source !== nothing # tracking a repo
         # make sure the package is registered so we have something to free to
         if isempty(registered_paths(ctx, pkg.uuid))
             pkgerror("cannot free package $(something(pkg.name, "")) since it is not found in a registry")
@@ -1320,11 +1320,12 @@ function sandbox(fn::Function, ctx::Context, target::PackageSpec, target_path::S
         end
         with_temp_env(tmp) do
             try
-                Pkg.API.develop(PackageSpec(;repo=GitRepo(;url=target_path)); strict=true)
+                Pkg.API.develop(PackageSpec(;repo=GitRepo(;source=target_path)); strict=true)
                 @debug "Using _parent_ dep graph"
-            catch # TODO
-                Base.rm(tmp_manifest) # retry with a clean dependency graph
-                Pkg.API.develop(PackageSpec(;repo=GitRepo(;url=target_path)))
+            catch err# TODO
+                @error err
+                isfile(tmp_manifest) && Base.rm(tmp_manifest) # retry with a clean dependency graph
+                Pkg.API.develop(PackageSpec(;repo=GitRepo(;source=target_path)))
                 @debug "Using _clean_ dep graph"
             end
             # Run sandboxed code

src/backwards_compatible_isolation.jl
@@ -1,7 +1,7 @@
 function _update_manifest(ctx::Context, pkg::PackageSpec, hash::Union{SHA1, Nothing})
     env = ctx.env
     uuid, name, version, path, special_action, repo = pkg.uuid, pkg.name, pkg.version, pkg.path, pkg.special_action, pkg.repo
-    hash === nothing && @assert (path != nothing || pkg.uuid in keys(ctx.stdlibs) || pkg.repo.url != nothing)
+    hash === nothing && @assert (path != nothing || pkg.uuid in keys(ctx.stdlibs) || pkg.repo.source != nothing)
     # TODO I think ^ assertion is wrong, add-repo should have a hash
     entry = get!(env.manifest, uuid, Types.PackageEntry())
     entry.name = name
@@ -12,23 +12,23 @@ function _update_manifest(ctx::Context, pkg::PackageSpec, hash::Union{SHA1, Noth
         entry.path = path
         if special_action == PKGSPEC_DEVELOPED
             entry.pinned = false
-            entry.repo.url = nothing
+            entry.repo.source = nothing
             entry.repo.rev = nothing
         elseif special_action == PKGSPEC_FREED
             if entry.pinned
                 entry.pinned = false
             else
-                entry.repo.url = nothing
+                entry.repo.source = nothing
                 entry.repo.rev = nothing
             end
         elseif special_action == PKGSPEC_PINNED
             entry.pinned = true
         elseif special_action == PKGSPEC_REPO_ADDED
-            entry.repo.url = repo.url
+            entry.repo.source = repo.source
             entry.repo.rev = repo.rev
             path = find_installed(name, uuid, hash)
         end
-        if entry.repo.url !== nothing
+        if entry.repo.source !== nothing
             path = find_installed(name, uuid, hash)
         end
     end
@@ -153,7 +153,7 @@ function _collect_fixed!(ctx::Context, pkgs::Vector{PackageSpec}, uuid_to_name::
             path = find_installed(pkg.name, pkg.uuid, pkg.tree_hash)
         elseif entry !== nothing && entry.path !== nothing
             path = pkg.path = entry.path
-        elseif entry !== nothing && entry.repo.url !== nothing
+        elseif entry !== nothing && entry.repo.source !== nothing
             path = find_installed(pkg.name, pkg.uuid, entry.tree_hash)
             pkg.repo = entry.repo
             pkg.tree_hash = entry.tree_hash
@@ -200,7 +200,7 @@ function apply_versions(ctx::Context, pkgs::Vector{PackageSpec}, hashes::Dict{UU
     for pkg in pkgs
         !is_stdlib(ctx, pkg.uuid) || continue
         pkg.path === nothing || continue
-        pkg.repo.url === nothing || continue
+        pkg.repo.source === nothing || continue
         path = find_installed(pkg.name, pkg.uuid, hashes[pkg.uuid])
         if !ispath(path)
             push!(pkgs_to_install, (pkg, path))
@@ -280,7 +280,7 @@ function apply_versions(ctx::Context, pkgs::Vector{PackageSpec}, hashes::Dict{UU
             uuid = pkg.uuid
             if pkg.path !== nothing || uuid in keys(ctx.stdlibs)
                 hash = nothing
-            elseif pkg.repo.url !== nothing
+            elseif pkg.repo.source !== nothing
                 hash = pkg.tree_hash
             else
                 hash = hashes[uuid]
@@ -322,7 +322,7 @@ function _version_data!(ctx::Context, pkgs::Vector{PackageSpec})
     clones = Dict{UUID,Vector{String}}()
     for pkg in pkgs
         !is_stdlib(pkg.uuid) || continue
-        pkg.repo.url === nothing || continue
+        pkg.repo.source === nothing || continue
         pkg.path === nothing || continue
         uuid = pkg.uuid
         ver = pkg.version::VersionNumber

src/manifest.jl
@@ -133,13 +133,13 @@ function Manifest(raw::Dict)::Manifest
         uuid = nothing
         deps = nothing
         try
-            uuid           = read_field("uuid",          nothing, info, safe_uuid)
-            entry.pinned   = read_pinned(get(info, "pinned", nothing))
-            entry.version  = read_field("version",       nothing, info, safe_version)
-            entry.path     = read_field("path",          nothing, info, safe_path)
-            entry.repo.url = read_field("repo-url",      nothing, info, identity)
-            entry.repo.rev = read_field("repo-rev",      nothing, info, identity)
-            entry.tree_hash = read_field("git-tree-sha1", nothing, info, safe_SHA1)
+            entry.pinned      = read_pinned(get(info, "pinned", nothing))
+            uuid              = read_field("uuid",          nothing, info, safe_uuid)
+            entry.version     = read_field("version",       nothing, info, safe_version)
+            entry.path        = read_field("path",          nothing, info, safe_path)
+            entry.repo.source = read_field("repo-url",      nothing, info, identity)
+            entry.repo.rev    = read_field("repo-rev",      nothing, info, identity)
+            entry.tree_hash   = read_field("git-tree-sha1", nothing, info, safe_SHA1)
             deps = read_deps(get(info, "deps", nothing))
         catch
             @error "Could not parse entry for `$name`"
@@ -201,7 +201,7 @@ function destructure(manifest::Manifest)::Dict
             path = join(splitpath(path), "/")
         end
         entry!(new_entry, "path", path)
-        entry!(new_entry, "repo-url", entry.repo.url)
+        entry!(new_entry, "repo-url", entry.repo.source)
         entry!(new_entry, "repo-rev", entry.repo.rev)
         if isempty(entry.deps)
             delete!(new_entry, "deps")

commit a519ca69b4bc762298efa024b5b496e65522530b

fix path resolution bug introduced in #1372

src/REPLMode/argument_parsers.jl
@@ -73,7 +73,7 @@ end
 # packages can be identified through: uuid, name, or name+uuid
 # additionally valid for add/develop are: local path, url
 function parse_package_identifier(word::AbstractString; add_or_develop=false)::PackageSpec
-    if add_or_develop && isdir_windows_workaround(expanduser(word))
+    if add_or_develop && (casesensitive_isdir(expanduser(word)) || word in (".", ".."))
         if !occursin(Base.Filesystem.path_separator_re, word)
             @info "Resolving package identifier `$word` as a directory at `$(Base.contractuser(abspath(word)))`."
         end

test/new.jl
@@ -840,6 +840,25 @@ end
         @test args == [Pkg.PackageSpec(;name="Example")]
         @test opts == Dict(:preserve => Pkg.PRESERVE_DIRECT)
     end
+    # check casesensitive resolution of paths
+    isolate() do; cd_tempdir() do dir
+        Pkg.REPLMode.TEST_MODE[] = true
+        # Add using UUID syntax
+        mkdir("example")
+        api, args, opts = first(Pkg.pkg"add Example")
+        @test api == Pkg.add
+        @test args == [Pkg.PackageSpec(;name="Example")]
+        @test isempty(opts)
+        api, args, opts = first(Pkg.pkg"add example")
+        @test api == Pkg.add
+        @test args == [Pkg.PackageSpec(;path="example")]
+        @test isempty(opts)
+        cd("example")
+        api, args, opts = first(Pkg.pkg"add .")
+        @test api == Pkg.add
+        @test args == [Pkg.PackageSpec(;path=".")]
+        @test isempty(opts)
+    end end
 end
 
 #