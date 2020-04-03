3f5c232afa542e0a61ca2c1ec4f4f9d04c24c6d9
Stefan Karpinski <stefan@karpinski.org>
2020-04-03 13:19:07 -0400

    wip

--- src/Download.jl
+++ src/Download.jl
@@ -10,11 +10,15 @@ import SHA: sha256
 """
     download(url, [ path ]; [ file_hash = <sha256> ]) -> path
 
-Download the file at `url`, saving the resulting download at `path`. If `path`
-is not provided, the file is saved to a temporary location which is returned. If
-the `file_hash` keyword argument is provided, the SHA2-256 hash of the
-downloaded file is computed and if it does not match the provided hash value,
-the path is deleted and an error is thrown.
+Download the file at `url`, saving the resulting download at `path`. All
+arguments are strings. If `path` is not provided, the file is saved to a
+temporary location which is returned. If the `file_hash` keyword argument is
+provided, the SHA2-256 hash of the downloaded file is computed and if it does
+not match the provided hash value, the path is deleted and an error is thrown.
+
+If `path` and `file_hash` are both provided and a file at that location already
+exists, the hash of the existing file will be computed and if it matches the
+given `file_hash`, no download is performed.
 """
 function download(
     url :: AbstractString,
@@ -56,19 +60,29 @@ end
     unpack(tarball, [ path ]; [ tree_hash = <sha1> ]) -> path
 
 Extract `tarball` to `path`, checking that the resulting tree has the git tree
-SHA1 hash provided (if any is). If `path` is already a tree, it will be checked
-for being the expected tree:
+SHA1 hash provided (if any is). All arguments are strings. When the tree is
+unpacked, the file `\$path/.tree_info.toml` is written containing these entries:
+
+* `git-tree-sha1`: the git tree SHA1 hash of the extracted tarball contents
+* `contents`: a table of extracted paths mapped to each file's type, one of
+  * `directory` — a directory
+  * `file` — a plain file, not executable
+  * `executable` — a file with the user executable bit set
+  * `symlink: \$link` — a symbolic link followed by the link target
+
+If `path` is already a tree and `tree_hash` is passed, the tree is checked:
 
 * If `\$path/.tree_info.toml` exists and is a valid TOML file with a top-level
   `git-tree-sha1` key, then the associated value is compared to `tree_hash` and
-  if they match the tree is presumed to have that hash and left as is.
+  if they match the tree is presumed to have that hash and nothing is done.
 
 * Otherwise, the existing tree at `path` is removed and replaced by the result
-  of extracting `path` and `\$path/.tree_info.toml` is written as well with the
-  value of `git-tree-sha1` set to the tree hash of the unpacked tree.
+  of extracting `path` where `\$path/.tree_info.toml` is written as described.
 
-If `tree_hash` is provided and the hash of the extracted tree does not match,
-then `path` is removed entirely and hash mismatch error is thrown.
+If `tree_hash` is provided and after extracting the given tarball its computed
+tree hash does not match the given hash value, `path` is deleted and an error
+is thrown indicating the hash mismatch. Thus, successful completion guarantees
+that the unpacked tree has the correct content.
 
 If `path` is not provided, `path = tempname()` is used and returned.
 """
@@ -77,12 +91,20 @@ function unpack(
     path :: AbstractString = tempname();
     tree_hash :: Union{AbstractString, Nothing} = nothing,
 )
+   unpack_core(() -> tarball, path, tree_hash = tree_hash)
+end
+
+function unpack_core(
+    get_tarball :: Function,
+    path :: AbstractString;
+    tree_hash :: Union{AbstractString, Nothing},
+)
     tree_info = joinpath(path, ".tree_info.toml")
     tree_hash = normalize_tree_hash(tree_hash)
     if tree_hash !== nothing && isdir(path)
         if isfile(tree_info)
             tree_info_hash = try
-                get(TOML.parsefile(), "git-tree-sha1", nothing)
+                get(TOML.parsefile(tree_info), "git-tree-sha1", nothing)
             catch err
                 @warn "invalid TOML" path=tree_info err
             end
@@ -90,15 +112,17 @@ function unpack(
         end
         rm(path, recursive=true)
     end
+    tarball = get_tarball()
     contents = Dict{String,String}()
+    symlinks = Dict{String,String}()
     open(`gzcat $tarball`) do io
         Tar.extract(io, path) do hdr
-            x = hdr.type == :file && (hdr.mode & 0o100) != 0
-            contents[hdr.path] = x ? "executable" : string(hdr.type)
+            executable = hdr.type == :file && (hdr.mode & 0o100) != 0
+            contents[hdr.path] = executable ? "executable" : string(hdr.type)
+            hdr.type == :symlink && (symlinks[hdr.path] = hdr.link)
             return true # extract everything
         end
     end
-    # TODO: use .tree_info.toml aware tree_hash?
     calc_hash = hash_tree(path)
     if tree_hash !== nothing && calc_hash != tree_hash
         msg  = "Tree hash mismatch!\n"
@@ -107,15 +131,21 @@ function unpack(
         rm(path, recursive=true)
         error(msg)
     end
-    if ispath(tree_info)
+    # make the TOML data structure
+    tree_info_data = Dict{String,Any}("git-tree-sha1" => calc_hash)
+    !isempty(contents) && (tree_info_data["contents"] = contents)
+    !isempty(symlinks) && (tree_info_data["symlinks"] = symlinks)
+    # if ".tree_info.toml" exists, warn and save its git hash
+    if haskey(contents, ".tree_info.toml") && ispath(tree_info)
         @warn "Overwriting extracted `.tree_info.toml`" path=tree_info
+        hash_func = (isdir(tree_info) ? GitTools.tree_hash : GitTools.blob_hash)
+        tree_info_data["git-path-sha1"] = Dict(
+            ".tree_info.toml" => bytes2hex(hash_func(tree_info))
+        )
         rm(tree_info, force=true, recursive=true)
     end
     open(tree_info, write=true) do io
-        TOML.print(io, sorted=true, Dict(
-            "git-tree-sha1" => calc_hash,
-            "contents" => contents,
-        ))
+        TOML.print(io, sorted=true, tree_info_data)
     end
     return path
 end
@@ -124,11 +154,13 @@ end
     download_unpack(url, [ path ];
         [ file_hash = <sha256> ], [ tree_hash = <sha1> ]) -> path
 
-Download the file at `url`, saving the resulting download at `path`. If `path`
-is not provided, the file is saved to a temporary location which is returned. If
-the `file_hash` keyword argument is provided, the SHA2-256 hash of the
-downloaded file is computed and if it does not match the provided hash value,
-the path is deleted and an error is thrown.
+Download the file at `url` and unpack its contents to `path`, checking the
+hash of both the downloaded file and the extracted tree if given. This is
+similar to doing `unpack(download(url; file_hash)); tree_hash)` except that
+if the output tree already exists with the correct tree hash, downloading
+is skipped and on error the downloaded temp file is deleted immediately.
+
+If `path` is not provided, `path = tempname()` is used and returned.
 """
 function download_unpack(
     url :: AbstractString,
@@ -136,11 +168,13 @@ function download_unpack(
     file_hash :: Union{AbstractString, Nothing} = nothing,
     tree_hash :: Union{AbstractString, Nothing} = nothing,
 )
-    # TODO: don't download if path is already correct
-    tarball = download(url, file_hash = file_hash)
-    try unpack(tarball, path, tree_hash = tree_hash)
+    try
+        tarball = nothing
+        unpack_core(path, tree_hash = tree_hash) do
+            tarball = download(url, file_hash = file_hash)
+        end
     catch
-        rm(tarball, force=true)
+        tarball !== nothing && rm(tarball, force=true)
         rethrow()
     end
 end
