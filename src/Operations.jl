73666f3b5a79e1da579c4e008541f9142b3dc983
Kristoffer Carlsson <kcarlsson89@gmail.com>
2020-04-14 15:47:29 +0200

    remove incorrect debug message

--- src/Operations.jl
+++ src/Operations.jl
@@ -251,9 +251,7 @@ function collect_developed!(ctx::Context, pkg::PackageSpec, developed::Vector{Pa
     source_ctx = Context(env = EnvCache(projectfile_path(source)))
     pkgs = load_all_deps(source_ctx)
     for pkg in filter(is_tracking_path, pkgs)
-        # Break eventual cycles
         if any(x -> x.uuid == pkg.uuid, developed)
-            @debug "found a cycle when collecting developed packages for $(pkg.name)"
             continue
         end
         # normalize path
