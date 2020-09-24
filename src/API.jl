a63bec1c4655dfce291c84996db2cb7670eae730
Ian <i.r.butterworth@gmail.com>
2020-09-24 01:21:23 -0400

    launch opt-in auto precompilation from appropriate gen funcs

--- src/API.jl
+++ src/API.jl
@@ -64,7 +64,10 @@ for f in (:develop, :add, :rm, :up, :pin, :free, :test, :build, :status)
     @eval begin
         $f(pkg::Union{AbstractString, PackageSpec}; kwargs...) = $f([pkg]; kwargs...)
         $f(pkgs::Vector{<:AbstractString}; kwargs...)          = $f([PackageSpec(pkg) for pkg in pkgs]; kwargs...)
-        $f(pkgs::Vector{PackageSpec}; kwargs...)               = $f(Context(), pkgs; kwargs...)
+        function $f(pkgs::Vector{PackageSpec}; kwargs...)
+            $f(Context(), pkgs; kwargs...)
+            ($(f in (:add, :up, :pin, :free, :build)) && _do_auto_precompile()) && Pkg.precompile()
+        end
         $f(ctx::Context; kwargs...) = $f(ctx, PackageSpec[]; kwargs...)
         function $f(; name::Union{Nothing,AbstractString}=nothing, uuid::Union{Nothing,String,UUID}=nothing,
                       version::Union{VersionNumber, String, VersionSpec, Nothing}=nothing,
@@ -186,7 +189,6 @@ function add(ctx::Context, pkgs::Vector{PackageSpec}; preserve::PreserveLevel=PR
     end
 
     Operations.add(ctx, pkgs, new_git; preserve=preserve, platform=platform)
-    _do_auto_precompile() && Pkg.precompile()
     return
 end
 
@@ -244,7 +246,6 @@ function up(ctx::Context, pkgs::Vector{PackageSpec};
         ensure_resolved(ctx, pkgs)
     end
     Operations.up(ctx, pkgs, level)
-    _do_auto_precompile() && Pkg.precompile()
     return
 end
 
@@ -1086,8 +1087,6 @@ function instantiate(ctx::Context; manifest::Union{Bool, Nothing}=nothing,
     Operations.download_artifacts(ctx, pkgs; platform=platform, verbose=verbose)
     # Run build scripts
     Operations.build_versions(ctx, union(UUID[pkg.uuid for pkg in new_apply], new_git); verbose=verbose)
-    
-    _do_auto_precompile() && Pkg.precompile()
 end
 
 
