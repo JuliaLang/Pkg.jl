1cb7baf3446cf6b72793202f4ea38dc951959677
Gunnar Farnebäck <gunnar.farneback@contextvision.se>
2020-11-30 11:39:38 +0100

    Use repo_source when cloning. (#2249)
    (cherry picked from commit ba2faa637b68a2389852973c97e946260674b0f9)

--- src/API.jl
+++ src/API.jl
@@ -1322,11 +1322,11 @@ function instantiate(ctx::Context; manifest::Union{Bool, Nothing}=nothing,
             pkgerror("Did not find path `$(repo_source)` for $(err_rep(pkg))")
         end
         repo_path = Types.add_repo_cache_path(repo_source)
-        LibGit2.with(GitTools.ensure_clone(ctx, repo_path, pkg.repo.source; isbare=true)) do repo
+        LibGit2.with(GitTools.ensure_clone(ctx, repo_path, repo_source; isbare=true)) do repo
             # We only update the clone if the tree hash can't be found
             tree_hash_object = tree_hash(repo, string(pkg.tree_hash))
             if tree_hash_object === nothing
-                GitTools.fetch(ctx, repo, pkg.repo.source; refspecs=Types.refspecs)
+                GitTools.fetch(ctx, repo, repo_source; refspecs=Types.refspecs)
                 tree_hash_object = tree_hash(repo, string(pkg.tree_hash))
             end
             if tree_hash_object === nothing
