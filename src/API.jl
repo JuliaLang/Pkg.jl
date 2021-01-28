009872d37688b64fa2908a1ff7798006e7d4a9e5
Ian <i.r.butterworth@gmail.com>
2021-01-28 16:50:09 -0500

    switch to `strict`

--- src/API.jl
+++ src/API.jl
@@ -980,7 +980,7 @@ function make_pkgspec(man, uuid)
 end
 
 precompile(; kwargs...) = precompile(Context(); kwargs...)
-function precompile(ctx::Context; internal_call::Bool=false, throw_indirect_deps::Bool=false, kwargs...)
+function precompile(ctx::Context; internal_call::Bool=false, strict::Bool=false, kwargs...)
     Context!(ctx; kwargs...)
     instantiate(ctx; allow_autoprecomp=false, kwargs...)
     time_start = time_ns()
@@ -1209,7 +1209,7 @@ function precompile(ctx::Context; internal_call::Bool=false, throw_indirect_deps
                         end
                     catch err
                         if err isa ErrorException
-                            failed_deps[pkg] = (throw_indirect_deps || is_direct_dep) ? String(take!(iob)) : ""
+                            failed_deps[pkg] = (strict || is_direct_dep) ? String(take!(iob)) : ""
                             !fancyprint && lock(print_lock) do
                                 println(io, string(color_string("  ✗ ", Base.error_color()), name))
                             end
@@ -1278,7 +1278,7 @@ function precompile(ctx::Context; internal_call::Bool=false, throw_indirect_deps
             err_str = ""
             n_direct_errs = 0
             for (dep, err) in failed_deps
-                if throw_indirect_deps || (dep in direct_deps)
+                if strict || (dep in direct_deps)
                     err_str *= "\n" * "$dep" * "\n\n" * err * (n_direct_errs > 0 ? "\n" : "")
                     n_direct_errs += 1
                 end
@@ -1286,7 +1286,7 @@ function precompile(ctx::Context; internal_call::Bool=false, throw_indirect_deps
             if err_str != ""
                 println(io, "")
                 plural = n_direct_errs == 1 ? "y" : "ies"
-                direct = throw_indirect_deps ? "" : "direct "
+                direct = strict ? "" : "direct "
                 pkgerror("The following $n_direct_errs $(direct)dependenc$(plural) failed to precompile:\n$(err_str[1:end-1])")
             end
         end
