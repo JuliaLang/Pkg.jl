09bdca205e89372de14365d1a4e2f3dd67915a2b
Ian <i.r.butterworth@gmail.com>
2021-01-30 21:14:18 -0500

    don't use fixed hash to find scratch space

--- test/new.jl
+++ test/new.jl
@@ -1808,8 +1808,9 @@ end
         log_file_add = joinpath(path, "deps", "build.log")
         @test_throws PkgError Pkg.add(path=path; io=devnull)
         @test !isfile(joinpath(Base.find_package("FailBuild"), "..", "..", "deps", "build.log"))
+        treehash = tree_hash(Base.find_package("FailBuild"))
         log_file_add = joinpath(DEPOT_PATH[1], "scratchspaces",
-            "44cfe95a-1eb2-52ea-b672-e2afdf69b78f", "f99d57aad0e5eb2434491b47bac92bb88d463001", "build.log")
+            "44cfe95a-1eb2-52ea-b672-e2afdf69b78f", treehash, "build.log")
         @test isdir(dirname(log_file_add))
         @test isfile(log_file_add)
         @test occursin("oops", read(log_file_add, String))
