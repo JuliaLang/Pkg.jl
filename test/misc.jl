8fda5db6153c976bbce744cdbf29ee6645dc991d
Ian <i.r.butterworth@gmail.com>
2021-05-08 14:26:34 -0400

    add tests for hash funcs

--- test/misc.jl
+++ test/misc.jl
@@ -7,3 +7,13 @@ using Pkg
     f() = Pkg.Types.UNREGISTERED_STDLIBS
     @inferred f()
 end
+
+@testset "hashing" begin
+    @test hash(Pkg.Types.Project()) == 0xd4b8cda960e4de88
+    @test hash(Pkg.Types.VersionBound()) == 0x873f51b414a315c2
+    @test hash(Pkg.Resolve.Fixed(VersionNumber(0,1,0))) == 0xea3ee20b2c9d6ccb
+
+    # hashes for these empty objects aren't stable
+    hash(Pkg.Types.VersionSpec())
+    hash(Pkg.Types.PackageEntry())
+end
