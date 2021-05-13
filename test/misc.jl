b0c256641a3abd533227b1b16d061ceba05fbebd
Ian <i.r.butterworth@gmail.com>
2021-05-12 20:07:58 -0400

    revert VersionSpec() test

--- test/misc.jl
+++ test/misc.jl
@@ -12,8 +12,7 @@ end
     @test hash(Pkg.Types.Project()) == 0xd4b8cda960e4de88
     @test hash(Pkg.Types.VersionBound()) == 0xdc46ceef40d7ff7f
     @test hash(Pkg.Resolve.Fixed(VersionNumber(0,1,0))) == 0xea3ee20b2c9d6ccb
-    @test hash(Pkg.Types.VersionSpec()) == 0x4245018905bae555
 
-    # hash isn't stable because the internal `repo` field is a mutable struct
-    hash(Pkg.Types.PackageEntry())
+    hash(Pkg.Types.VersionSpec()) # hash isn't stable
+    hash(Pkg.Types.PackageEntry()) # hash isn't stable because the internal `repo` field is a mutable struct
 end
