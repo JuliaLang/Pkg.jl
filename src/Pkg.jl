c5b1f3dcda61dd31a562d6b4376611b42911a804
Elliot Saba <staticfloat@gmail.com>
2021-05-25 05:17:49 -0700

    Converge `Pkg.gc` and `Pkg.API.gc` docs (#2566)

--- src/Pkg.jl
+++ src/Pkg.jl
@@ -222,11 +222,14 @@ by starting julia with `--inline=no`.
 const test = API.test
 
 """
-    Pkg.gc(; io::IO=stderr)
+    Pkg.gc(; collect_delay::Period=Day(7), io::IO=stderr)
 
-Garbage collect packages that are no longer reachable from any project.
-Only packages that are tracked by version are deleted, so no packages
-that might contain local changes are touched.
+Garbage-collect package and artifact installations by sweeping over all known
+`Manifest.toml` and `Artifacts.toml` files, noting those that have been deleted, and then
+finding artifacts and packages that are thereafter not used by any other projects,
+marking them as "orphaned".  This method will only remove orphaned objects (package
+versions, artifacts, and scratch spaces) that have been continually un-used for a period
+of `collect_delay`; which defaults to seven days.
 """
 const gc = API.gc
 
