1ec8a609f25e273e7773fc03cef19f9afaa8b571
Dilum Aluthge <dilum@aluthge.com>
2021-06-06 20:31:43 -0400

    CI: Use the same alternate UUID every time (#2595)
    Co-authored-by: Ian Butterworth <i.r.butterworth@gmail.com>
    
    Co-authored-by: Ian Butterworth <i.r.butterworth@gmail.com>

--- .ci/change_uuid.jl
+++
@@ -1,5 +0,0 @@
-using UUIDs
-
-project_filename = joinpath(dirname(@__DIR__), "Project.toml")
-
-write("Project.toml", replace(read("Project.toml", String), r"uuid = .*?\n" =>"uuid = \"$(uuid4())\"\n"))
