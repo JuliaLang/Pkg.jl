4246e3d5ee2d28b1793e5f301cf4cddf88dbc7ef
Ian <i.r.butterworth@gmail.com>
2021-07-13 00:03:55 -0400

    only keep latest usage per object

--- src/Types.jl
+++ src/Types.jl
@@ -459,34 +459,57 @@ end
 function write_env_usage(source_file::AbstractString, usage_filepath::AbstractString)
     # Don't record ghost usage
     !isfile(source_file) && return
-
     # Ensure that log dir exists
     !ispath(logdir()) && mkpath(logdir())
 
-    # Generate entire entry as a string first
-    entry = sprint() do io
-        TOML.print(io, Dict(source_file => [Dict("time" => now())]))
-    end
+    p = TOML.Parser()
+    usage_file = joinpath(logdir(), usage_filepath)
+    timestamp = now()
 
     ## Atomically write usage file
-    usage_file = joinpath(logdir(), usage_filepath)
     while true
         # read existing usage file
-        existing_usage = isfile(usage_file) ? read(usage_file, String) : ""
+        usage = if isfile(usage_file)
+            content = read(usage_file)
+            Base.TOML.reinit!(p, String(content); filepath=usage_file)
+            Base.TOML.parse(p)
+        else
+            Dict()
+        end
+
+        # record new usage
+        usage[source_file] = [Dict("time" => timestamp)]
+
+        # keep only latest usage info
+        for kv in usage
+            times = map(d->Dates.DateTime(d["time"]), usage[kv.first])
+            usage[kv.first] = [Dict("time" => maximum(times))]
+        end
 
         # Write to a temp file in the same directory as the destination
         temp_usage_file = tempname(logdir())
         open(temp_usage_file, "w") do io
-            write(io, existing_usage)
-            write(io, entry)
+            TOML.print(io, usage, sorted=true)
         end
 
         # Move the temp file into place, replacing the original
         mv(temp_usage_file, usage_file, force = true)
 
         # Check that the new file has what we want in it
-        new_usage = read(usage_file, String)
-        occursin(entry, new_usage) && break
+        new_usage = if isfile(usage_file)
+            content = read(usage_file)
+            Base.TOML.reinit!(p, String(content); filepath=usage_file)
+            Base.TOML.parse(p)
+        else
+            Dict()
+        end
+        if haskey(new_usage, source_file)
+            for e in new_usage[source_file]
+                if Dates.DateTime(e["time"]) >= timestamp
+                    return
+                end
+            end
+        end
         # If not, try again
     end
 
