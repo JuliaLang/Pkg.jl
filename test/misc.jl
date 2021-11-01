6f8e36db88ed0c7727b5616b2baa58787cdc4e95
Ian Butterworth <i.r.butterworth@gmail.com>
2021-10-31 22:32:39 -0400

    Fix two `Base.hash` methods and add tests (#2556)

--- test/misc.jl
+++ test/misc.jl
@@ -7,3 +7,12 @@ using Pkg
     f() = Pkg.Types.UNREGISTERED_STDLIBS
     @inferred f()
 end
+
+@testset "hashing" begin
+    @test hash(Pkg.Types.Project()) == hash(Pkg.Types.Project())
+    @test hash(Pkg.Types.VersionBound()) == hash(Pkg.Types.VersionBound())
+    @test hash(Pkg.Resolve.Fixed(VersionNumber(0,1,0))) == hash(Pkg.Resolve.Fixed(VersionNumber(0,1,0)))
+
+    hash(Pkg.Types.VersionSpec()) # hash isn't stable
+    hash(Pkg.Types.PackageEntry()) # hash isn't stable because the internal `repo` field is a mutable struct
+end
