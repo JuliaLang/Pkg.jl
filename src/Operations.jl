b0f668244d64e89d6300c6ec2d6d88cd71d77af4
Mosè Giordano <giordano@users.noreply.github.com>
2022-01-11 18:11:01 +0100

    Fix `UndefVarError` in `Pkg.Operations.download_source` (#2932)
    (cherry picked from commit c313966330e161d5fc8218d0144b80884b6d5657, PR#2932)

--- src/Operations.jl
+++ src/Operations.jl
@@ -819,7 +819,12 @@ function download_source(ctx::Context, pkgs::Vector{PackageSpec},
                 str = sprint(; context=ctx.io) do io
                     if success
                         fancyprint && print_progress_bottom(io)
-                        vstr = pkg.version !== nothing ? "v$(pkg.version)" : "[$h]"
+                        vstr = if pkg.version !== nothing
+                            "v$(pkg.version)"
+                        else
+                            short_treehash = string(pkg.tree_hash)[1:16]
+                            "[$short_treehash]"
+                        end
                         printpkgstyle(io, :Installed, string(rpad(pkg.name * " ", max_name + 2, "─"), " ", vstr))
                         fancyprint && show_progress(io, bar)
                     end
@@ -839,7 +844,12 @@ function download_source(ctx::Context, pkgs::Vector{PackageSpec},
         uuid = pkg.uuid
         install_git(ctx, pkg.uuid, pkg.name, pkg.tree_hash, urls[uuid], pkg.version::VersionNumber, path)
         readonly && set_readonly(path)
-        vstr = pkg.version !== nothing ? "v$(pkg.version)" : "[$h]"
+        vstr = if pkg.version !== nothing
+            "v$(pkg.version)"
+        else
+            short_treehash = string(pkg.tree_hash)[1:16]
+            "[$short_treehash]"
+        end
         printpkgstyle(ctx, :Installed, string(rpad(pkg.name * " ", max_name + 2, "─"), " ", vstr))
     end
 
