0f7167a12dc897d59c8332588663b3a67a84f2b0
Ian Butterworth <i.r.butterworth@gmail.com>
2022-01-17 01:07:10 -0500

    Make interrupting `Pkg.test` more graceful (#2933)

--- src/Operations.jl
+++ src/Operations.jl
@@ -1674,10 +1674,27 @@ function test(ctx::Context, pkgs::Vector{PackageSpec};
             printpkgstyle(ctx.io, :Testing, "Running tests...")
             flush(ctx.io)
             cmd = gen_test_code(testfile(source_path); coverage=coverage, julia_args=julia_args, test_args=test_args)
-            p = run(pipeline(ignorestatus(cmd), stdout = sandbox_ctx.io))
+            p = run(pipeline(ignorestatus(cmd), stdin = stdin, stdout = sandbox_ctx.io, stderr = stderr_f()), wait = false)
+            interrupted = false
+            try
+                wait(p)
+            catch e
+                if e isa InterruptException
+                    interrupted = true
+                    print("\n")
+                    printpkgstyle(ctx.io, :Testing, "Tests interrupted. Exiting the test process\n", color = Base.error_color())
+                    # Give some time for the child interrupt handler to print a stacktrace and exit,
+                    # then kill the process if still running
+                    if timedwait(() -> !process_running(p), 4) == :timed_out
+                        kill(p, Base.SIGKILL)
+                    end
+                else
+                    rethrow()
+                end
+            end
             if success(p)
                 printpkgstyle(ctx.io, :Testing, pkg.name * " tests passed ")
-            else
+            elseif !interrupted
                 push!(pkgs_errored, (pkg.name, p))
             end
         end
