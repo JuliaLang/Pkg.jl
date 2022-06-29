8059565cf835e06dfe1018df53a3f5f7836176b8
t-bltg <tf.bltg@gmail.com>
2022-06-29 06:00:49 +0200

    update warning msg
    Co-authored-by: Ian Butterworth <i.r.butterworth@gmail.com>

--- src/REPLMode/REPLMode.jl
+++ src/REPLMode/REPLMode.jl
@@ -227,7 +227,7 @@ end
 function tokenize(cmd::String)
     cmd = replace(replace(cmd, "\r\n" => "; "), "\n" => "; ") # for multiline commands
     if startswith(cmd, ']')
-        @warn "Removing leading `]`"
+        @warn "Removing leading `]`, which should only be used once to switch to pkg> mode"
         cmd = string(lstrip(cmd, ']'))
     end
     qstrings = lex(cmd)
