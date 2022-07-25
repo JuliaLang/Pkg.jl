8b26121e37e4b22bb64964406a49fcd15357907b
Gabriel Baraldi <baraldigabriel@gmail.com>
2022-07-24 21:12:17 -0300

    Switch to redirect_stdio from logging

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,43 +21,47 @@ end
 hide_logs = Pkg.get_bool_env("JULIA_PKG_TEST_QUIET", default="true")
 
 logdir = get(ENV, "JULIA_TEST_VERBOSE_LOGS_DIR", nothing)
-### Send all Pkg output to a BufferStream if true (default)
-hide_stdoutstderr = hide_logs
+### Send all Pkg output to a file called Pkg.log
 
-Pkg.DEFAULT_IO[] = hide_stdoutstderr ? Base.BufferStream() : stdout
+islogging = logdir !== nothing
+
+if islogging
+    logfile = joinpath(logdir, "Pkg.log")
+    Pkg.DEFAULT_IO[] = open(logfile, "a")
+elseif hide_logs
+    Pkg.DEFAULT_IO[] = devnull
+else
+    Pkg.DEFAULT_IO[] = stdout
+end
+
 Pkg.REPLMode.minirepl[] = Pkg.REPLMode.MiniREPL() # re-set this given DEFAULT_IO has changed
 
 include("utils.jl")
 
-Logging.with_logger(hide_logs ? Logging.NullLogger() : Logging.current_logger()) do
+redirect_stdio(stdout=Pkg.DEFAULT_IO[], stderr=Pkg.DEFAULT_IO[]) do
     @testset "Pkg" begin
-        @testset "$f" for f in [
-            "new.jl",
-            "pkg.jl",
-            "repl.jl",
-            "api.jl",
-            "registry.jl",
-            "subdir.jl",
-            "artifacts.jl",
-            "binaryplatforms.jl",
-            "platformengines.jl",
-            "sandbox.jl",
-            "resolve.jl",
-            "misc.jl",
-            "force_latest_compatible_version.jl",
-            "manifests.jl",
-            ]
-            @info "==== Testing `test/$f`"
-            flush(Pkg.DEFAULT_IO[])
-            include(f)
-        end
-    end
-end
+                @testset "$f" for f in [
+                    "new.jl",
+                    "pkg.jl",
+                    "repl.jl",
+                    "api.jl",
+                    "registry.jl",
+                    "subdir.jl",
+                    "artifacts.jl",
+                    "binaryplatforms.jl",
+                    "platformengines.jl",
+                    "sandbox.jl",
+                    "resolve.jl",
+                    "misc.jl",
+                    "force_latest_compatible_version.jl",
+                    "manifests.jl",
+                    ]
+                    @info "==== Testing `test/$f`"
+                    flush(Pkg.DEFAULT_IO[])
+                    include(f)
+                end
+            islogging && close(Pkg.DEFAULT_IO[])
 
-if logdir !== nothing
-    logfile = joinpath(logdir, "Pkg.log")
-    open(logfile, "a") do io
-        write(io, Pkg.DEFAULT_IO[])
     end
 end
 
