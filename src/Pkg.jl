81f7aa719a3467e1c44b037421b8e25ccefa9220
t-bltg <tf.bltg@gmail.com>
2022-11-04 16:57:40 +0100

    update `PackageInfo` and `ProjectInfo` docs (#3149)
    Co-authored-by: Ian Butterworth <i.r.butterworth@gmail.com>

--- src/Pkg.jl
+++ src/Pkg.jl
@@ -356,15 +356,20 @@ The result is a `Dict` that maps a package UUID to a `PackageInfo` struct repres
 
 # `PackageInfo` fields
 
-| Field             | Description                                                |
-|:------------------|:-----------------------------------------------------------|
-| `name`            | The name of the package                                    |
-| `version`         | The version of the package (this is `Nothing` for stdlibs) |
-| `is_direct_dep`   | The package is a direct dependency                         |
-| `is_tracking_path`| Whether a package is directly tracking a directory         |
-| `is_pinned`       | Whether a package is pinned                                |
-| `source`          | The directory containing the source code for that package  |
-| `dependencies`    | The dependencies of that package as a vector of UUIDs      |
+| Field                 | Description                                                                       |
+|:------------------    |:----------------------------------------------------------------------------------|
+| `name`                | The name of the package                                                           |
+| `version`             | The version of the package (this is `Nothing` for stdlibs)                        |
+| `tree_hash`           | A file hash of the package directory tree                                         |
+| `is_direct_dep`       | The package is a direct dependency                                                |
+| `is_pinned`           | Whether a package is pinned                                                       |
+| `is_tracking_path`    | Whether a package is tracking a path                                              |
+| `is_tracking_repo`    | Whether a package is tracking a repository                                        |
+| `is_tracking_registry`| Whether a package is being tracked by registry i.e. not by path nor by repository |
+| `git_revision`        | The git revision when tracking by repository                                      |
+| `git_source`          | The git source when tracking by repository                                        |
+| `source`              | The directory containing the source code for that package                         |
+| `dependencies`        | The dependencies of that package as a vector of UUIDs                             |
 """
 const dependencies = API.dependencies
 
@@ -382,6 +387,7 @@ Request a `ProjectInfo` struct which contains information about the active proje
 | name         | The project's name                                                                          |
 | uuid         | The project's UUID                                                                          |
 | version      | The project's version                                                                       |
+| ispackage    | Whether the project is a package (has a name and uuid)                                      |
 | dependencies | The project's direct dependencies as a `Dict` which maps dependency name to dependency UUID |
 | path         | The location of the project file which defines the active project                           |
 """
