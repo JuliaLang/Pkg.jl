20ea9885b9ff0b56407d26957d80d9ccc0ba2bf7
Jacob Quinn <quinn.jacobd@gmail.com>
2022-11-28 12:25:56 -0700

    debug

--- src/Operations.jl
+++ src/Operations.jl
@@ -268,6 +268,7 @@ function collect_fixed!(env::EnvCache, pkgs::Vector{PackageSpec}, names::Dict{UU
     for pkg in pkgs
         # add repo package if necessary
         if pkg.repo.rev !== nothing
+            @show pkg
             # ensure revved package is installed
             Types.handle_repo_add!(Types.Context(env=env), pkg)
         end
