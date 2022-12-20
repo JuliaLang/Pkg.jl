b3115d984ffe0b74b29d7e7ac1e435988459a324
Ian <i.r.butterworth@gmail.com>
2022-12-20 16:03:21 -0500

    fix ref

--- test/test_packages/ExtensionExamples/HasExtensions.jl/test/runtests.jl
+++ test/test_packages/ExtensionExamples/HasExtensions.jl/test/runtests.jl
@@ -2,9 +2,9 @@ using HasExtensions
 using Test
 
 @test !HasExtensions.offsetarrays_loaded
-@test foo(Int[1,2,3]) == 1
+@test HasExtensions.foo(rand(Float64, 2)) == 1
 
 using OffsetArrays
 
 @test HasExtensions.offsetarrays_loaded
-@test foo(OffsetArray(Int[1,2,3], -1:1)) == 2
+@test HasExtensions.foo(OffsetArray(rand(Float64, 2), 0:1)) == 2
