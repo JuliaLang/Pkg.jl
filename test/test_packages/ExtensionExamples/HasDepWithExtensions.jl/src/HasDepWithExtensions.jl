e0cf922cf94541e3129c7ce4eec25ba555a69dc4
Ian <i.r.butterworth@gmail.com>
2022-12-20 16:33:57 -0500

    actually extend parent methods in weakdep examples

--- test/test_packages/ExtensionExamples/HasDepWithExtensions.jl/src/HasDepWithExtensions.jl
+++ test/test_packages/ExtensionExamples/HasDepWithExtensions.jl/src/HasDepWithExtensions.jl
@@ -6,10 +6,10 @@ using OffsetArrays: OffsetArray
 
 function do_something()
     # @info "First do something with the basic array support in B"
-    HasExtensions.foo(rand(Float64, 2))
+    HasExtensions.foo(rand(Float64, 2)) == 1 || error("Unexpected value")
 
     # @info "Now do something with extended OffsetArray support in B"
-    HasExtensions.foo(OffsetArray(rand(Float64, 2), 0:1))
+    HasExtensions.foo(OffsetArray(rand(Float64, 2), 0:1)) == 2 || error("Unexpected value")
 end
 
 end # module
