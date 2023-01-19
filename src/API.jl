a325470c2d71f5013e7769f7eadd2dc4385d5c34
Ian Butterworth <i.r.butterworth@gmail.com>
2023-01-19 16:43:00 -0500

    Precompile: tidier state cache file handling (#3323)

--- src/API.jl
+++ src/API.jl
@@ -869,10 +869,18 @@ function gc(ctx::Context=Context(); collect_delay::Period=Day(7), verbose=false,
                 uuid_dir = joinpath(scratchdir, uuid)
                 !isdir(uuid_dir) && continue
                 for space in readdir(uuid_dir)
-                    space_dir = joinpath(uuid_dir, space)
-                    !isdir(space_dir) && continue
-                    if !(space_dir in spaces_to_keep)
-                        push!(depot_orphaned_scratchspaces, space_dir)
+                    space_dir_or_file = joinpath(uuid_dir, space)
+                    if isdir(space_dir_or_file)
+                        if !(space_dir_or_file in spaces_to_keep)
+                            push!(depot_orphaned_scratchspaces, space_dir_or_file)
+                        end
+                    elseif uuid == Operations.PkgUUID && isfile(space_dir_or_file)
+                        # special cleanup for the precompile cache files that Pkg saves
+                        if any(prefix->startswith(basename(space_dir_or_file), prefix), ("suspend_cache_", "pending_cache_"))
+                            if mtime(space_dir_or_file) < (time() - (24*60*60))
+                                push!(depot_orphaned_scratchspaces, space_dir_or_file)
+                            end
+                        end
                     end
                 end
             end
@@ -928,7 +936,16 @@ function gc(ctx::Context=Context(); collect_delay::Period=Day(7), verbose=false,
 
     # Delete paths for unreachable package versions and artifacts, and computing size saved
     function delete_path(path)
-        path_size = recursive_dir_size(path)
+        path_size = if isfile(path)
+            try
+                lstat(path).size
+            catch ex
+                @error("Failed to calculate size of $path", exception=ex)
+                0
+            end
+        else
+            recursive_dir_size(path)
+        end
         try
             Base.Filesystem.prepare_for_deletion(path)
             Base.rm(path; recursive=true, force=true)
@@ -1532,9 +1549,13 @@ function save_precompile_state()
     path = Operations.pkg_scratchpath()
     for (prefix, store) in (("suspend_cache_", pkgs_precompile_suspended), ("pending_cache_", pkgs_precompile_pending))
         fpath = joinpath(path, string(prefix, hash(string(Base.active_project(), Base.VERSION))))
-        mkpath(path); Base.Filesystem.rm(fpath, force=true)
-        open(fpath, "w") do io
-            serialize(io, store)
+        if isempty(store)
+            Base.rm(fpath, force=true)
+        else
+            mkpath(path); Base.rm(fpath, force=true)
+            open(fpath, "w") do io
+                serialize(io, store)
+            end
         end
     end
     return nothing
@@ -1551,6 +1572,7 @@ function recall_precompile_state()
                     empty!(store)
                 end
             end
+            Base.rm(fpath, force=true)
         else
             empty!(store)
         end
