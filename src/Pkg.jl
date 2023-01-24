128df6ba34880ec619dbeafce0ff93ebb295076e
Ian <i.r.butterworth@gmail.com>
2023-01-24 14:41:32 -0500

    change compat to 1.9

--- src/Pkg.jl
+++ src/Pkg.jl
@@ -165,8 +165,8 @@ Set `timing=true` to show the duration of the precompilation of each dependency.
 !!! compat "Julia 1.8"
     Specifying packages to precompile requires at least Julia 1.8.
 
-!!! compat "Julia 1.10"
-    Timing mode requires Julia 1.10.
+!!! compat "Julia 1.9"
+    Timing mode requires Julia 1.9.
 
 # Examples
 ```julia
