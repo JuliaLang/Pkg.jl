b5199d2798185aecca3f46c7d397e0808502a8f7
t-bltg <tf.bltg@gmail.com>
2023-05-07 22:02:40 +0200

    keep `path` & `repo` when `pinned` (#3148)

--- src/Pkg.jl
+++ src/Pkg.jl
@@ -312,7 +312,9 @@ const build = API.build
     Pkg.pin(pkgs::Union{PackageSpec, Vector{PackageSpec}}; io::IO=stderr, all_pkgs::Bool=false)
 
 Pin a package to the current version (or the one given in the `PackageSpec`) or to a certain
-git revision. A pinned package is never updated. To pin all dependencies set `all_pkgs=true`.
+git revision. A pinned package is never automatically updated: if `pkg` is tracking a path,
+or a repository, those remain tracked but will not update. 
+To get updates from the origin path or remote repository the package must first be freed.
 
 !!! compat "Julia 1.7"
     The `all_pkgs` kwarg was introduced in julia 1.7.
--- test/pkg.jl
+++ test/pkg.jl
@@ -18,7 +18,7 @@ import LibGit2
 
 using ..Utils
 
-const TEST_PKG = (name = "Example", uuid = UUID("7876af07-990d-54b4-ab0e-23690620f79a"))
+const TEST_PKG = (name = "Example", uuid = UUID("7876af07-990d-54b4-ab0e-23690620f79a"), url = "https://github.com/JuliaLang/Example.jl")
 const PackageSpec = Pkg.Types.PackageSpec
 
 import Pkg.Types: semver_spec, VersionSpec
@@ -550,7 +550,7 @@ temp_pkg_dir() do project_path; cd(project_path) do
     cd(tmp) do; @testset "instantiating updated repo" begin
         empty!(DEPOT_PATH)
         pushfirst!(DEPOT_PATH, depo1)
-        LibGit2.close(LibGit2.clone("https://github.com/JuliaLang/Example.jl", "Example.jl"))
+        LibGit2.close(LibGit2.clone(TEST_PKG.url, "Example.jl"))
         mkdir("machine1")
         cd("machine1")
         Pkg.activate(".")
@@ -937,6 +937,79 @@ end
 @testset "Issue #3069" begin
     p = PackageSpec(; path="test_packages/Example")
     @test_throws Pkg.Types.PkgError("Package PackageSpec(\n  path = test_packages/Example\n  version = *\n) has neither name nor uuid") ensure_resolved(Pkg.Types.Context(), Pkg.Types.Manifest(), [p])
+end
+
+@testset "Issue #3147" begin
+    prev_project = Base.active_project()
+
+    @testset "Pkg.add" begin
+        Pkg.activate(temp = true)
+        mktempdir() do tmp_dir
+            LibGit2.close(LibGit2.clone(TEST_PKG.url, tmp_dir))
+            Pkg.develop(path=tmp_dir)
+            Pkg.pin("Example")
+            Pkg.add("Example")
+            info = Pkg.dependencies()[TEST_PKG.uuid]
+            @test info.is_pinned
+            @test info.is_tracking_path
+            @test !info.is_tracking_repo
+            @test info.version > v"0.5.3"
+        end
+        Pkg.rm("Example")
+
+        Pkg.add(url=TEST_PKG.url, rev="29aa1b4")
+        Pkg.pin("Example")
+        Pkg.add("Example")
+        info = Pkg.dependencies()[TEST_PKG.uuid]
+        @test info.is_pinned
+        @test !info.is_tracking_path
+        @test info.is_tracking_repo
+        @test info.version == v"0.5.3"
+        Pkg.rm("Example")
+    end
+
+    @testset "Pkg.update" begin
+        Pkg.activate(temp = true)
+        mktempdir() do tmp_dir
+            ver = v"0.5.3"
+            repo = LibGit2.clone(TEST_PKG.url, tmp_dir)
+            tag = LibGit2.GitObject(repo, "v$ver")
+            hash = string(LibGit2.target(tag))
+            LibGit2.checkout!(repo, hash)
+            LibGit2.close(repo)
+            Pkg.develop(path=tmp_dir)
+            Pkg.pin("Example")
+            Pkg.update("Example")  # pkg should remain pinned
+            info = Pkg.dependencies()[TEST_PKG.uuid]
+            @test info.is_pinned
+            @test info.is_tracking_path
+            @test !info.is_tracking_repo
+            @test info.version == ver
+
+            # modify the pkg version manually, to mimic developing this pkg
+            dev_ver = VersionNumber(ver.major, ver.minor, ver.patch + 1)
+            fn = joinpath(tmp_dir, "Project.toml")
+            toml = TOML.parse(read(fn, String))
+            toml["version"] = string(dev_ver)
+            open(io -> TOML.print(io, toml), fn, "w")
+            Pkg.update("Example")  # noop since Pkg.is_fully_pinned(...) is true
+            info = Pkg.dependencies()[TEST_PKG.uuid]
+            @test info.is_pinned
+            @test info.is_tracking_path
+            @test !info.is_tracking_repo
+            @test info.version == ver
+
+            Pkg.pin("Example")  # pinning a 2ⁿᵈ time updates versions in the manifest
+            info = Pkg.dependencies()[TEST_PKG.uuid]
+            @test info.is_pinned
+            @test info.is_tracking_path
+            @test !info.is_tracking_repo
+            @test info.version == dev_ver
+      end
+      Pkg.rm("Example")
+    end
+
+    Pkg.activate(prev_project)
 end
 
 end # module
