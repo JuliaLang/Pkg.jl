cb550db4f5a18e6c818649fbc32276ce2b0294c2
Ian Butterworth <i.r.butterworth@gmail.com>
2023-08-08 13:49:17 -0400

    print given logs are captured to file

--- test/utils.jl
+++ test/utils.jl
@@ -85,7 +85,7 @@ function isolate(fn::Function; loaded_depot=false, linked_reg=true)
                     try
                         Base.rm(target_depot; force=true, recursive=true)
                     catch err
-                        @warn "isolate failed to clean up depot" err
+                        println("warning: isolate failed to clean up depot.\n  $err")
                     end
                 end
             end
