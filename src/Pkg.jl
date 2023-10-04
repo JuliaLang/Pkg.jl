6ed8f3132385533adb39a18b8244d65285d37ed5
Sebastian Pfitzner <pfitzseb@gmail.com>
2023-10-04 15:25:15 +0200

    adjust JULIA_PKG_OFFLINE doc string

--- src/Pkg.jl
+++ src/Pkg.jl
@@ -592,8 +592,8 @@ In offline mode Pkg tries to do as much as possible without connecting
 to internet. For example, when adding a package Pkg only considers
 versions that are already downloaded in version resolution.
 
-To work in offline mode across Julia sessions you can
-set the environment variable `JULIA_PKG_OFFLINE` to `"true"`.
+To work in offline mode across Julia sessions you can set the environment 
+variable `JULIA_PKG_OFFLINE` to `"true"` before starting Julia.
 """
 offline(b::Bool=true) = (OFFLINE_MODE[] = b; nothing)
 
