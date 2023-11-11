debc38b9a2d800f035a17f42a01c14a3775fa859
Kristoffer Carlsson <kcarlsson89@gmail.com>
2023-11-11 05:23:11 +0100

    remove recompilation dependency check in Pkg.precompile (#3696)
    Co-authored-by: Ian Butterworth <i.r.butterworth@gmail.com>

--- src/API.jl
+++ src/API.jl
@@ -1079,11 +1079,11 @@ function precompile(ctx::Context, pkgs::Vector{PackageSpec}; internal_call::Bool
                     strict::Bool=false, warn_loaded = true, already_instantiated = false, timing::Bool = false,
                     _from_loading::Bool=false, kwargs...)
     Context!(ctx; kwargs...)
-    if !already_instantiated 
+    if !already_instantiated
         instantiate(ctx; allow_autoprecomp=false, kwargs...)
         @debug "precompile: instantiated"
     end
-        
+
     time_start = time_ns()
 
     # Windows sometimes hits a ReadOnlyMemoryError, so we halve the default number of tasks. Issue #2323
@@ -1497,10 +1497,8 @@ function precompile(ctx::Context, pkgs::Vector{PackageSpec}; internal_call::Bool
                 queued = precomp_queued(pkgspec)
 
                 circular = pkg in circular_deps
-                # skip stale checking and force compilation if any dep was recompiled in this session
-                any_dep_recompiled = any(map(dep->was_recompiled[dep], deps))
                 is_stale = true
-                if !circular && (queued || any_dep_recompiled || (!suspended && (is_stale = !Base.isprecompiled(pkg; ignore_loaded=true, stale_cache, cachepaths, sourcepath))))
+                if !circular && (queued || (!suspended && (is_stale = !Base.isprecompiled(pkg; ignore_loaded=true, stale_cache, cachepaths, sourcepath))))
                     Base.acquire(parallel_limiter)
                     is_direct_dep = pkg in direct_deps
 
