6f91ea96b82e5323e506d8443ba0852ceb6de0e0
Gunnar Farnebäck <gunnar.farneback@contextvision.se>
2023-11-20 22:29:20 +0100

    Revise the API of is_manifest_current.

--- src/Pkg.jl
+++ src/Pkg.jl
@@ -720,12 +720,17 @@ Upgrades the format of the current or specified manifest file from v1.0 to v2.0 
 const upgrade_manifest = API.upgrade_manifest
 
 """
-    is_manifest_current(ctx::Context = Context())
+    is_manifest_current(path::AbstractString)
 
-Returns whether the active manifest was resolved from the active project state.
+Returns whether the manifest for the project at `path` was resolved from the current project file.
 For instance, if the project had compat entries changed, but the manifest wasn't re-resolved, this would return false.
 
-If the manifest doesn't have the project hash recorded, `nothing` is returned.
+If the manifest doesn't have the project hash recorded, or if there is no manifest file, `nothing` is returned.
+
+This function can be used in tests to verify that the manifest is synchronized with the project file:
+
+    using Pkg, Test, Package
+    @test Pkg.is_manifest_current(pkgdir(Package))
 """
 const is_manifest_current = API.is_manifest_current
 
