82a61d9b224ff64a2534fdcc26fe35ac8f4e0680
Ian Butterworth <i.r.butterworth@gmail.com>
2023-12-13 11:52:53 -0500

    fix non-reverting cd in precompile script

--- src/precompile.jl
+++ src/precompile.jl
@@ -6,95 +6,96 @@ using REPL
 let
     function _run_precompilation_script_setup()
         tmp = mktempdir()
-        cd(tmp)
-        empty!(DEPOT_PATH)
-        pushfirst!(DEPOT_PATH, tmp)
-        pushfirst!(LOAD_PATH, "@")
-        write(
-            "Project.toml",
-            """
-            name = "Hello"
-            uuid = "33cfe95a-1eb2-52ea-b672-e2afdf69b78f"
-            """,
-        )
-        mkdir("src")
-        write(
-            "src/Hello.jl",
-            """
-            module Hello
+        cd(tmp) do
+            empty!(DEPOT_PATH)
+            pushfirst!(DEPOT_PATH, tmp)
+            pushfirst!(LOAD_PATH, "@")
+            write(
+                "Project.toml",
+                """
+                name = "Hello"
+                uuid = "33cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+                """,
+            )
+            mkdir("src")
+            write(
+                "src/Hello.jl",
+                """
+                module Hello
+                end
+                """,
+            )
+            Pkg.activate(".")
+            Pkg.generate("TestPkg")
+            uuid = TOML.parsefile(joinpath("TestPkg", "Project.toml"))["uuid"]
+            mv("TestPkg", "TestPkg.jl")
+            tree_hash = cd("TestPkg.jl") do
+                sig = LibGit2.Signature("TEST", "TEST@TEST.COM", round(time()), 0)
+                repo = LibGit2.init(".")
+                LibGit2.add!(repo, "")
+                commit =
+                    LibGit2.commit(repo, "initial commit"; author = sig, committer = sig)
+                th =
+                    LibGit2.peel(LibGit2.GitTree, LibGit2.GitObject(repo, commit)) |>
+                    LibGit2.GitHash |>
+                    string
+                close(repo)
+                th
             end
-            """,
-        )
-        Pkg.activate(".")
-        Pkg.generate("TestPkg")
-        uuid = TOML.parsefile(joinpath("TestPkg", "Project.toml"))["uuid"]
-        mv("TestPkg", "TestPkg.jl")
-        tree_hash = cd("TestPkg.jl") do
-            sig = LibGit2.Signature("TEST", "TEST@TEST.COM", round(time()), 0)
-            repo = LibGit2.init(".")
-            LibGit2.add!(repo, "")
-            commit =
-                LibGit2.commit(repo, "initial commit"; author = sig, committer = sig)
-            th =
-                LibGit2.peel(LibGit2.GitTree, LibGit2.GitObject(repo, commit)) |>
-                LibGit2.GitHash |>
-                string
-            close(repo)
-            th
+            # Prevent cloning the General registry by adding a fake one
+            mkpath("registries/Registry/T/TestPkg")
+            write(
+                "registries/Registry/Registry.toml",
+                """
+                name = "Registry"
+                uuid = "37c07fec-e54c-4851-934c-2e3885e4053e"
+                repo = "https://github.com/JuliaRegistries/Registry.git"
+                [packages]
+                $uuid = { name = "TestPkg", path = "T/TestPkg" }
+                """,
+            )
+            write(
+                "registries/Registry/T/TestPkg/Compat.toml",
+                """
+                ["0"]
+                julia = "1"
+                """,
+            )
+            write(
+                "registries/Registry/T/TestPkg/Deps.toml",
+                """
+                ["0"]
+                Dates = "ade2ca70-3891-5945-98fb-dc099432e06a"
+                """,
+            )
+            write(
+                "registries/Registry/T/TestPkg/Versions.toml",
+                """
+                ["0.1.0"]
+                git-tree-sha1 = "$tree_hash"
+                """,
+            )
+            write(
+                "registries/Registry/T/TestPkg/Package.toml",
+                """
+                name = "TestPkg"
+                uuid = "$uuid"
+                repo = "$(escape_string(tmp))/TestPkg.jl"
+                """,
+            )
+            Tar.create("registries/Registry", "registries/Registry.tar")
+            cmd = `$(Pkg.PlatformEngines.exe7z()) a "registries/Registry.tar.gz" -tgzip "registries/Registry.tar"`
+            run(pipeline(cmd, stdout = stdout_f(), stderr = stderr_f()))
+            write(
+                "registries/Registry.toml",
+                """
+                git-tree-sha1 = "11b5fad51c4f98cfe0c145ceab0b8fb63fed6f81"
+                uuid = "37c07fec-e54c-4851-934c-2e3885e4053e"
+                path = "Registry.tar.gz"
+                """,
+            )
+            Base.rm("registries/Registry"; recursive = true)
         end
-        # Prevent cloning the General registry by adding a fake one
-        mkpath("registries/Registry/T/TestPkg")
-        write(
-            "registries/Registry/Registry.toml",
-            """
-            name = "Registry"
-            uuid = "37c07fec-e54c-4851-934c-2e3885e4053e"
-            repo = "https://github.com/JuliaRegistries/Registry.git"
-            [packages]
-            $uuid = { name = "TestPkg", path = "T/TestPkg" }
-            """,
-        )
-        write(
-            "registries/Registry/T/TestPkg/Compat.toml",
-            """
-            ["0"]
-            julia = "1"
-            """,
-        )
-        write(
-            "registries/Registry/T/TestPkg/Deps.toml",
-            """
-            ["0"]
-            Dates = "ade2ca70-3891-5945-98fb-dc099432e06a"
-            """,
-        )
-        write(
-            "registries/Registry/T/TestPkg/Versions.toml",
-            """
-            ["0.1.0"]
-            git-tree-sha1 = "$tree_hash"
-            """,
-        )
-        write(
-            "registries/Registry/T/TestPkg/Package.toml",
-            """
-            name = "TestPkg"
-            uuid = "$uuid"
-            repo = "$(escape_string(tmp))/TestPkg.jl"
-            """,
-        )
-        Tar.create("registries/Registry", "registries/Registry.tar")
-        cmd = `$(Pkg.PlatformEngines.exe7z()) a "registries/Registry.tar.gz" -tgzip "registries/Registry.tar"`
-        run(pipeline(cmd, stdout = stdout_f(), stderr = stderr_f()))
-        write(
-            "registries/Registry.toml",
-            """
-            git-tree-sha1 = "11b5fad51c4f98cfe0c145ceab0b8fb63fed6f81"
-            uuid = "37c07fec-e54c-4851-934c-2e3885e4053e"
-            path = "Registry.tar.gz"
-            """,
-        )
-        Base.rm("registries/Registry"; recursive = true)
         return tmp
     end
 
