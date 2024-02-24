3ff7ff3a56456d55aa66381d1326cb8f710f072b
Ian Butterworth <i.r.butterworth@gmail.com>
2024-02-24 13:57:07 -0500

    use more of Pkg's precompile harness

--- ext/REPLExt/precompile.jl
+++ ext/REPLExt/precompile.jl
@@ -10,14 +10,24 @@ let
     REPL.raw!(::FakeTerminal, raw::Bool) = raw
 
     function pkgreplmode_precompile()
+        original_depot_path = copy(DEPOT_PATH)
+        original_load_path = copy(LOAD_PATH)
         __init__()
         Pkg.UPDATED_REGISTRY_THIS_SESSION[] = true
-        try_prompt_pkg_add(Symbol[:notapackage])
-        promptf()
-        term = FakeTerminal()
-        repl = REPL.LineEditREPL(term, true)
-        REPL.run_repl(repl)
-        repl_init(repl)
+        Pkg.DEFAULT_IO[] = Pkg.UnstableIO(devnull)
+        withenv("JULIA_PKG_SERVER" => nothing, "JULIA_PKG_UNPACK_REGISTRY" => nothing) do
+            tmp = Pkg._run_precompilation_script_setup()
+            cd(tmp) do
+                try_prompt_pkg_add(Symbol[:notapackage])
+                promptf()
+                term = FakeTerminal()
+                repl = REPL.LineEditREPL(term, true)
+                REPL.run_repl(repl)
+                repl_init(repl)
+            end
+        end
+        copy!(DEPOT_PATH, original_depot_path)
+        copy!(LOAD_PATH, original_load_path)
     end
 
     if Base.generating_output()
