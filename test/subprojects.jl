c251627de168c13398601ec0454921ab6376c9b6
Kristoffer <kcarlsson89@gmail.com>
2024-03-13 17:24:21 +0100

    - more tests - allow nested subprojects - simplify things by not differentiating between the base project and the sub projects in EnvCache

--- test/subprojects.jl
+++ test/subprojects.jl
@@ -4,57 +4,107 @@ import ..Pkg # ensure we are using the correct Pkg
 using Test
 using TOML
 using UUIDs
-
-tmp = mktempdir()
-cd(tmp) do
-    name = "MonorepoSub"
-    rm(name, force=true, recursive=true)
-    Pkg.generate(name)
-    cd("MonorepoSub")
-    Pkg.activate(".")
-    Pkg.add("Example")
-    Pkg.add("Crayons")
-    Pkg.compat("Crayons", "=4.0.0, =4.0.1, =4.0.2, =4.0.3")
-    Pkg.generate("PrivatePackage")
-    Pkg.develop(path="PrivatePackage")
-
-    d = TOML.parsefile("Project.toml")
-    d["subprojects"] = ["test", "docs", "benchmarks"]
-    d["sources"] = Dict("PrivatePackage" => Dict("path" => "PrivatePackage"))
-    Pkg.Types.write_project(d, "Project.toml")
-    # Add tests
-    mkdir("test")
-    Pkg.generate("TestSpecificPackage")
-    Pkg.activate("test")
-    Pkg.add("Test")
-    Pkg.add("Crayons")
-    Pkg.compat("Crayons", "=4.0.1, =4.0.2, =4.0.3, =4.0.4")
-    # Compat in base package should prevent updating to 4.0.4
-    Pkg.update()
-    @test Pkg.dependencies()[UUID("a8cc5b0e-0ffa-5ad4-8c14-923d3ee1735f")].version == v"4.0.3"
-
-    Pkg.develop(path="TestSpecificPackage")
-    d = TOML.parsefile("test/Project.toml")
-    d["sources"] = Dict("TestSpecificPackage" => Dict("path" => "TestSpecificPackage"))
-    Pkg.Types.write_project(d, "test/Project.toml")
-
-    @test !isfile("test/Manifest.toml")
-    write("test/runtests.jl", """
-        using Test # subproject specific
-        using PrivatePackage # base project specific with source
-        using TestSpecificPackage # subproject specific with source
-        using MonorepoSub # base project
-        using Example # base project dependency
-    """)
-    @test success(run(`$(Base.julia_cmd()) --startup-file=no --project="test" test/runtests.jl`))
-
-    # Test loading the package itself
-    @test success(run(`$(Base.julia_cmd()) --startup-file=no --project -e 'using MonorepoSub'`))
-
-    rm("Manifest.toml")
-    Pkg.resolve()
-    @test success(run(`$(Base.julia_cmd()) --startup-file=no --project="test" test/runtests.jl`))
-    @test success(run(`$(Base.julia_cmd()) --startup-file=no --project -e 'using MonorepoSub'`))
+if !isdefined(@__MODULE__, :Utils)
+    include("utils.jl")
+    using .Utils
 end
 
+
+temp_pkg_dir() do project_path
+    cd(project_path) do
+        name = "MonorepoSub"
+        rm(name, force=true, recursive=true)
+        Pkg.generate(name)
+        cd("MonorepoSub") do
+            Pkg.activate(".")
+            # Add Example, Crayons, PrivatePackage to the "MonorepoSub" package
+            Pkg.add("Example")
+            Pkg.add(;name="Crayons", version="v4.0.3")
+            Pkg.compat("Crayons", "=4.0.0, =4.0.1, =4.0.2, =4.0.3")
+            Pkg.generate("PrivatePackage")
+            Pkg.develop(path="PrivatePackage")
+            d = TOML.parsefile("Project.toml")
+            d["subprojects"] = ["test", "docs", "benchmarks", "PrivatePackage"]
+            abs_path = abspath("PrivatePackage") # TODO: Make relative after #3842 is fixed
+            d["sources"] = Dict("PrivatePackage" => Dict("path" => abs_path))
+            Pkg.Types.write_project(d, "Project.toml")
+            write("src/MonorepoSub.jl", """
+                module MonorepoSub
+                using Example, Crayons, PrivatePackage
+                end
+            """)
+
+            # Add some deps to PrivatePackage
+            Pkg.activate("PrivatePackage")
+            Pkg.add("Chairmarks")
+            @test !isfile("PrivatePackage/Manifest.toml")
+            d = TOML.parsefile("PrivatePackage/Project.toml")
+            d["subprojects"] = ["test"]
+            Pkg.Types.write_project(d, "PrivatePackage/Project.toml")
+            write("PrivatePackage/src/PrivatePackage.jl", """
+            module PrivatePackage
+                using Chairmarks
+            end
+            """)
+
+            # Make a test subproject in PrivatePackage
+            # Note that this is a "nested subproject" since in this environment
+            # PrivatePackage is a subproject of MonorepoSub
+            mkdir("PrivatePackage/test")
+            Pkg.activate("PrivatePackage/test")
+            # This adds too many packages to the Project file...
+            Pkg.add("Test")
+            @test length(Pkg.project().dependencies) == 1
+            write("PrivatePackage/test/runtests.jl", """
+                using Test # nested subproject specific
+                using PrivatePackage # subproject package
+                using Chairmarks # base project dependency
+            """)
+            # A nested subproject should still use the root base manifest
+            @test !isfile("PrivatePackage/test/Manifest.toml")
+
+            # Add tests to MonorepoSub
+            mkdir("test")
+            Pkg.activate("test")
+            # Test specific deps
+            Pkg.add("Test")
+            Pkg.add("Crayons")
+            Pkg.compat("Crayons", "=4.0.1, =4.0.2, =4.0.3, =4.0.4")
+            # Compat in base package should prevent updating to 4.0.4
+            Pkg.update()
+            @test Pkg.dependencies()[UUID("a8cc5b0e-0ffa-5ad4-8c14-923d3ee1735f")].version == v"4.0.3"
+            Pkg.generate("TestSpecificPackage")
+            Pkg.develop(path="TestSpecificPackage")
+            d = TOML.parsefile("test/Project.toml")
+            abs_pkg = abspath("TestSpecificPackage") # TODO: Make relative after #3842 is fixed
+            d["sources"] = Dict("TestSpecificPackage" => Dict("path" => abs_pkg))
+            Pkg.Types.write_project(d, "test/Project.toml")
+
+            @test !isfile("test/Manifest.toml")
+            write("test/runtests.jl", """
+                using Test # subproject specific
+                using PrivatePackage # base project specific with source
+                using TestSpecificPackage # subproject specific with source
+                using MonorepoSub # base project
+                using Example # base project dependency
+            """)
+
+            # Test that the subprojects are working
+            @test success(run(`$(Base.julia_cmd()) --startup-file=no --project="test" test/runtests.jl`))
+            @test success(run(`$(Base.julia_cmd()) --startup-file=no --project -e 'using MonorepoSub'`))
+            @test success(run(`$(Base.julia_cmd()) --startup-file=no --project="PrivatePackage" -e 'using PrivatePackage'`))
+            @test success(run(`$(Base.julia_cmd()) --startup-file=no --project="PrivatePackage/test" PrivatePackage/test/runtests.jl`))
+
+            rm("Manifest.toml")
+            Pkg.activate(".")
+            Pkg.resolve()
+            # Resolve should have fixed the manifest so that everything above works from the existing project files
+            @test success(run(`$(Base.julia_cmd()) --startup-file=no --project="test" test/runtests.jl`))
+            @test success(run(`$(Base.julia_cmd()) --startup-file=no --project -e 'using MonorepoSub'`))
+            @test success(run(`$(Base.julia_cmd()) --startup-file=no --project="PrivatePackage" -e 'using PrivatePackage'`))
+            @test success(run(`$(Base.julia_cmd()) --startup-file=no --project="PrivatePackage/test" PrivatePackage/test/runtests.jl`))
+        end
+    end
 end
+
+end # module
