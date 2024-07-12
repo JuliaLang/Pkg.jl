fd4fa5aefd443eee0d319b1bc8a55155314dcdcb
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-07-12 10:50:10 -0500

    Mark API public (#3945)
    (cherry picked from commit 3c6e3982b8a308e20d3d7b2f542a4ecd22a75d1d)

--- src/Pkg.jl
+++ src/Pkg.jl
@@ -17,6 +17,9 @@ export UpgradeLevel, UPLEVEL_MAJOR, UPLEVEL_MINOR, UPLEVEL_PATCH
 export PreserveLevel, PRESERVE_TIERED_INSTALLED, PRESERVE_TIERED, PRESERVE_ALL_INSTALLED, PRESERVE_ALL, PRESERVE_DIRECT, PRESERVE_SEMVER, PRESERVE_NONE
 export Registry, RegistrySpec
 
+public activate, add, build, compat, develop, free, gc, generate, instantiate, 
+       pin, precompile, redo, rm, resolve, status, test, undo, update, why
+
 depots() = Base.DEPOT_PATH
 function depots1()
     d = depots()
