0646715e28c0a5496a2e2405452496c9fd2b07f8
Ian Butterworth <i.r.butterworth@gmail.com>
2025-01-10 11:39:10 -0500

    remove the `delete!` with a comment

--- src/Operations.jl
+++ src/Operations.jl
@@ -510,7 +510,9 @@ function resolve_versions!(env::EnvCache, registries::Vector{Registry.RegistryIn
             new_v = vers_fix[uuid]
             compat_map[uuid][old_v] = compat_map[uuid][new_v]
             vers_fix[uuid] = old_v
-            new_v != old_v && delete!(compat_map[uuid], new_v)
+            # Note that we don't delete!(compat_map[uuid], old_v) because we want to keep the compat info around
+            # in case there's JLL version confusion between the sysimage pkgorigins version and manifest
+            # but that issue hasn't been fully specified, so keep it to be cautious
         end
     end
     vers = vers_fix
