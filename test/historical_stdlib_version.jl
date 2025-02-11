74ef75721f809d1c40388aab5ecbbe12aed1937d
Ian Butterworth <i.r.butterworth@gmail.com>
2025-02-10 21:10:11 -0500

    use temp envs for each test

--- test/historical_stdlib_version.jl
+++ test/historical_stdlib_version.jl
@@ -201,45 +201,53 @@ libcxxwrap_julia_jll_UUID = Base.UUID("3eaa8342-bff7-56a5-9981-c04077f7cee7")
 libblastrampoline_jll_UUID = Base.UUID("8e850b90-86db-534c-a0d3-1478176c7d93")
 
 isolate(loaded_depot=true) do
-    Pkg.activate(temp=true)
     @testset "Elliot and Mosè's mini Pkg test suite" begin # https://github.com/JuliaPackaging/JLLPrefixes.jl/issues/6
         HistoricalStdlibVersions.register!()
         @testset "Standard add" begin
+            Pkg.activate(temp=true)
             # Standard add (non-stdlib, flexible version)
             Pkg.add(; name="HelloWorldC_jll")
             @test haskey(Pkg.dependencies(), HelloWorldC_jll_UUID)
 
+            Pkg.activate(temp=true)
             # Standard add (non-stdlib, url and rev)
             Pkg.add(; name="HelloWorldC_jll", url="https://github.com/JuliaBinaryWrappers/HelloWorldC_jll.jl", rev="0b4959a49385d4bb00efd281447dc19348ebac08")
             @test Pkg.dependencies()[Base.UUID("dca1746e-5efc-54fc-8249-22745bc95a49")].git_revision === "0b4959a49385d4bb00efd281447dc19348ebac08"
 
+            Pkg.activate(temp=true)
             # Standard add (non-stdlib, specified version)
             Pkg.add(; name="HelloWorldC_jll", version=v"1.0.10+1")
             @test Pkg.dependencies()[Base.UUID("dca1746e-5efc-54fc-8249-22745bc95a49")].version === v"1.0.10+1"
 
+            Pkg.activate(temp=true)
             # Standard add (non-stdlib, versionspec)
             Pkg.add(; name="HelloWorldC_jll", version=Pkg.Types.VersionSpec("1.0.10"))
             @test Pkg.dependencies()[Base.UUID("dca1746e-5efc-54fc-8249-22745bc95a49")].version === v"1.0.10+1"
         end
 
         @testset "Julia-version-dependent add" begin
+            Pkg.activate(temp=true)
             # Julia-version-dependent add (non-stdlib, flexible version)
             Pkg.add(; name="libcxxwrap_julia_jll", julia_version=v"1.7")
             @test Pkg.dependencies()[libcxxwrap_julia_jll_UUID].version === v"0.12.1+0"
 
+            Pkg.activate(temp=true)
             # Julia-version-dependent add (non-stdlib, specified version)
             Pkg.add(; name="libcxxwrap_julia_jll", version=v"0.9.4+0", julia_version=v"1.7")
             @test Pkg.dependencies()[libcxxwrap_julia_jll_UUID].version === v"0.9.4+0"
 
+            Pkg.activate(temp=true)
             Pkg.add(; name="libcxxwrap_julia_jll", version=v"0.8.8+1", julia_version=v"1.9")
             @test Pkg.dependencies()[libcxxwrap_julia_jll_UUID].version === v"0.8.8+1"
         end
 
         @testset "Stdlib add" begin
+            Pkg.activate(temp=true)
             # Stdlib add (current julia version)
             Pkg.add(; name="GMP_jll")
             @test Pkg.dependencies()[GMP_jll_UUID].version >= v"6.3.0+2" # v1.13.0-DEV
 
+            Pkg.activate(temp=true)
             # Stdlib add (other julia version)
             Pkg.add(; name="GMP_jll", julia_version=v"1.7")
             @test Pkg.dependencies()[GMP_jll_UUID].version === v"6.2.1+1"
@@ -253,11 +261,13 @@ isolate(loaded_depot=true) do
             # then install that.  If we have to manually work around that and look up what
             # GMP_jll for Julia v1.7 is, then ask for that version explicitly, that's ok.
 
+            Pkg.activate(temp=true)
             Pkg.add(; name="GMP_jll", julia_version=v"1.7")
 
             # This is expected to fail, that version can't live with `julia_version = v"1.7"`
             @test_throws Pkg.Resolve.ResolverError Pkg.add(; name="GMP_jll", version=v"6.2.0+5", julia_version=v"1.7")
 
+            Pkg.activate(temp=true)
             # Stdlib add (julia_version == nothing)
             # Note: this is currently known to be broken, we get the wrong GMP_jll!
             Pkg.add(; name="GMP_jll", version=v"6.2.1+1", julia_version=nothing)
@@ -265,6 +275,7 @@ isolate(loaded_depot=true) do
         end
 
         @testset "julia_version = nothing" begin
+            Pkg.activate(temp=true)
             # Stdlib add (impossible constraints due to julia version compat, so
             # must pass `julia_version=nothing`). In this case, we always fully
             # specify versions, but if we don't, it's okay to just give us whatever
