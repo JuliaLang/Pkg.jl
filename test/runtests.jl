c54ee594d1c16270d4f67e09788839d9618faf18
Ian Butterworth <i.r.butterworth@gmail.com>
2025-02-13 17:48:08 -0500

    tests: make sure we're in an active project and that it's clean (#4155)

--- test/runtests.jl
+++ test/runtests.jl
@@ -76,6 +76,7 @@ Logging.with_logger((islogging || Pkg.DEFAULT_IO[] == devnull) ? Logging.Console
     end
 
     @testset "Pkg" begin
+        Pkg.activate(; temp=true) # make sure we're in an active project and that it's clean
         try
         @testset "$f" for f in test_files
                 @info "==== Testing `test/$f`"
