e79a49106abe07e5903e7a282f4f641cc38233aa
Jesper Stemann Andersen <jesper@sait.dk>
2025-02-24 16:04:57 +0100

    Revert "Added `@test try catch end` to count Pkg.test invocations as tests"
    This reverts commit dfa6bbdee2980793a2a3fa1a8e4f37614258f07d.

--- test/new.jl
+++ test/new.jl
@@ -2026,61 +2026,36 @@ end
                     other_nthreads_default = default_nthreads_default == 1 ? 2 : 1
                     other_nthreads_interactive = default_nthreads_interactive == 0 ? 1 : 0
                     @testset "default" begin
-                        @test try
-                            Pkg.test("TestThreads"; julia_args=Cmd(``; env=Dict(
-                                "EXPECTED_NUM_THREADS_DEFAULT" => "$default_nthreads_default",
-                                "EXPECTED_NUM_THREADS_INTERACTIVE" => "$default_nthreads_interactive",
-                            )))
-                            true
-                        catch
-                            false
-                        end
+                        Pkg.test("TestThreads"; julia_args=Cmd(``; env=Dict(
+                            "EXPECTED_NUM_THREADS_DEFAULT" => "$default_nthreads_default",
+                            "EXPECTED_NUM_THREADS_INTERACTIVE" => "$default_nthreads_interactive",
+                        )))
                     end
                     @testset "JULIA_NUM_THREADS=other_nthreads_default" begin
-                        @test try
-                            Pkg.test("TestThreads"; julia_args=Cmd(``; env=Dict(
-                                "EXPECTED_NUM_THREADS_DEFAULT" => "$other_nthreads_default",
-                                "EXPECTED_NUM_THREADS_INTERACTIVE" => "$default_nthreads_interactive",
-                                "JULIA_NUM_THREADS" => "$other_nthreads_default",
-                            )))
-                            true
-                        catch
-                            false
-                        end
+                        Pkg.test("TestThreads"; julia_args=Cmd(``; env=Dict(
+                            "EXPECTED_NUM_THREADS_DEFAULT" => "$other_nthreads_default",
+                            "EXPECTED_NUM_THREADS_INTERACTIVE" => "$default_nthreads_interactive",
+                            "JULIA_NUM_THREADS" => "$other_nthreads_default",
+                        )))
                     end
                     @testset "JULIA_NUM_THREADS=other_nthreads_default,other_nthreads_interactive" begin
-                        @test try
-                            Pkg.test("TestThreads"; julia_args=Cmd(``; env=Dict(
-                                "EXPECTED_NUM_THREADS_DEFAULT" => "$other_nthreads_default",
-                                "EXPECTED_NUM_THREADS_INTERACTIVE" => "$other_nthreads_interactive",
-                                "JULIA_NUM_THREADS" => "$other_nthreads_default,$other_nthreads_interactive",
-                            )))
-                            true
-                        catch
-                            false
-                        end
+                        Pkg.test("TestThreads"; julia_args=Cmd(``; env=Dict(
+                            "EXPECTED_NUM_THREADS_DEFAULT" => "$other_nthreads_default",
+                            "EXPECTED_NUM_THREADS_INTERACTIVE" => "$other_nthreads_interactive",
+                            "JULIA_NUM_THREADS" => "$other_nthreads_default,$other_nthreads_interactive",
+                        )))
                     end
                     @testset "--threads=other_nthreads_default" begin
-                        @test try
-                            Pkg.test("TestThreads"; julia_args=Cmd(` --threads=$other_nthreads_default`; env=Dict(
-                                "EXPECTED_NUM_THREADS_DEFAULT" => "$other_nthreads_default",
-                                "EXPECTED_NUM_THREADS_INTERACTIVE" => "$default_nthreads_interactive",
-                            )))
-                            true
-                        catch
-                            false
-                        end
+                        Pkg.test("TestThreads"; julia_args=Cmd(` --threads=$other_nthreads_default`; env=Dict(
+                            "EXPECTED_NUM_THREADS_DEFAULT" => "$other_nthreads_default",
+                            "EXPECTED_NUM_THREADS_INTERACTIVE" => "$default_nthreads_interactive",
+                        )))
                     end
                     @testset "--threads=other_nthreads_default,other_nthreads_interactive" begin
-                        @test try
-                            Pkg.test("TestThreads"; julia_args=Cmd(` --threads=$other_nthreads_default,$other_nthreads_interactive`; env=Dict(
-                                "EXPECTED_NUM_THREADS_DEFAULT" => "$other_nthreads_default",
-                                "EXPECTED_NUM_THREADS_INTERACTIVE" => "$other_nthreads_interactive",
-                            )))
-                            true
-                        catch
-                            false
-                        end
+                        Pkg.test("TestThreads"; julia_args=Cmd(` --threads=$other_nthreads_default,$other_nthreads_interactive`; env=Dict(
+                            "EXPECTED_NUM_THREADS_DEFAULT" => "$other_nthreads_default",
+                            "EXPECTED_NUM_THREADS_INTERACTIVE" => "$other_nthreads_interactive",
+                        )))
                     end
                 end
             end
