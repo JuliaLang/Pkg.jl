b50f202f99407e529cbc08d37329273c101db1cc
Ian Butterworth <i.r.butterworth@gmail.com>
2025-03-31 12:17:45 -0400

    don't warn if people are opting out of the pkgserver explicitly

--- src/Registry/Registry.jl
+++ src/Registry/Registry.jl
@@ -451,7 +451,9 @@ function update(regs::Vector{RegistrySpec}; io::IO=stderr_f(), force::Bool=true,
                                     registry_update_log[string(reg.uuid)] = now()
                                     @label done_tarball_read
                                 else
-                                    if reg.name == "General" && Base.get_bool_env("JULIA_PKG_GEN_REG_FMT_CHECK", true)
+                                    if reg.name == "General" &&
+                                            Base.get_bool_env("JULIA_PKG_GEN_REG_FMT_CHECK", true) &&
+                                            get(ENV, "JULIA_PKG_SERVER", "") == ""
                                         @info """
                                             The General registry is installed via unpacked tarball.
                                             Consider reinstalling it via the newer faster direct from
@@ -478,7 +480,9 @@ function update(regs::Vector{RegistrySpec}; io::IO=stderr_f(), force::Bool=true,
                         end
                     elseif isdir(joinpath(reg.path, ".git"))
                         printpkgstyle(io, :Updating, "registry at " * regpath)
-                        if reg.name == "General" && Base.get_bool_env("JULIA_PKG_GEN_REG_FMT_CHECK", true)
+                        if reg.name == "General" &&
+                                Base.get_bool_env("JULIA_PKG_GEN_REG_FMT_CHECK", true) &&
+                                get(ENV, "JULIA_PKG_SERVER", "") == ""
                             @info """
                                 The General registry is installed via git. Consider reinstalling it via
                                 the newer faster direct from tarball format by running:
