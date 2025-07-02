04e17e7d4b786f3b1abb093ebecef8f7e6efa0ad
KristofferC <kristoffer.carlsson@juliacomputing.com>
2025-07-02 12:33:43 +0200

    fix copilot suggestion

--- test/test_packages/Rot13.jl/src/Rot13.jl
+++ test/test_packages/Rot13.jl/src/Rot13.jl
@@ -2,7 +2,7 @@ module Rot13
 
 function rot13(c::Char)
     shft = islowercase(c) ? 'a' : 'A'
-    return isletter(c) ? c = shft + (c - shft + 13) % 26 : c
+    return isletter(c) ? shft + (c - shft + 13) % 26 : c
 end
 
 rot13(str::AbstractString) = map(rot13, str)
