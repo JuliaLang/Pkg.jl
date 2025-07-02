0dd632e3a4aea7cb9ed5481afcbc7401cb5dd54e
Ian Butterworth <i.r.butterworth@gmail.com>
2025-07-02 23:55:55 +0300

    Update src/generate.jl

--- src/generate.jl
+++ src/generate.jl
@@ -66,12 +66,14 @@ function entrypoint(io::IO, pkg::AbstractString, dir)
 
             \"""
                 hello(who::String)
+            
             Return "Hello, `who`".
             \"""
             hello(who::String) = "Hello, \$who"
 
             \"""
                 domath(x::Number)
+            
             Return `x + 5`.
             \"""
             domath(x::Number) = x + 5
