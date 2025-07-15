440991c0b83c6e1e9d481c9b6b536e84a3451a3f
Ian Butterworth <i.r.butterworth@gmail.com>
2025-07-15 12:08:47 -0400

    Update registries during `dev` (#4186)
    (cherry picked from commit 0e123a2034fa9b995d875e540c420823e372c67c)

--- src/API.jl
+++ src/API.jl
@@ -244,6 +244,7 @@ function develop(ctx::Context, pkgs::Vector{PackageSpec}; shared::Bool=true,
 
     new_git = handle_repos_develop!(ctx, pkgs, shared)
 
+    Operations.update_registries(ctx; force = false, update_cooldown = Day(1))
 
     for pkg in pkgs
         if Types.collides_with_project(ctx.env, pkg)
