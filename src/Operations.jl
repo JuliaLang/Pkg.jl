e749a514020e45d71362ba55cf86e764feaece18
Kristoffer Carlsson <kcarlsson89@gmail.com>
2025-09-12 16:50:48 +0200

    improve warning message when computed hash is not the same as the one recorded in Artifacts. (#4385)

--- src/Operations.jl
+++ src/Operations.jl
@@ -842,8 +842,9 @@ function install_archive(
             unpacked = joinpath(dir, dirs[1])
         end
         # Assert that the tarball unpacked to the tree sha we wanted
-        if SHA1(GitTools.tree_hash(unpacked)) != hash
-            @warn "tarball content does not match git-tree-sha1"
+        computed_hash = GitTools.tree_hash(unpacked)
+        if SHA1(computed_hash) != hash
+            @warn "tarball content of url $url does not match git-tree-sha1, expected $hash, got $computed_hash"
             url_success = false
         end
         url_success || continue
