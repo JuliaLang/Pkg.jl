b98a092ce07e6961fe58c5ea288b9cc07cb9b5d3
Ian Butterworth <i.r.butterworth@gmail.com>
2025-10-06 23:57:43 -0400

    Make julia compat checking respect julia_version (#4403)
    (cherry picked from commit 92aae600c1c645bd46bc5937d47337ac7093d194)

--- test/api.jl
+++ test/api.jl
@@ -308,8 +308,8 @@ end
     isolate(loaded_depot=true) do; mktempdir() do tempdir
         pathf = git_init_package(tempdir, joinpath(@__DIR__, "test_packages", "FarFuture"))
         pathp = git_init_package(tempdir, joinpath(@__DIR__, "test_packages", "FarPast"))
-        @test_throws "julia version requirement from Project.toml's compat section not satisfied for package" Pkg.add(path=pathf)
-        @test_throws "julia version requirement from Project.toml's compat section not satisfied for package" Pkg.add(path=pathp)
+        @test_throws "julia version requirement for package" Pkg.add(path=pathf)
+        @test_throws "julia version requirement for package" Pkg.add(path=pathp)
     end end
 end
 
