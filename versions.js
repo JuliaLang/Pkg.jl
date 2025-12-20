b30da34cba3212ef5f5d94555c49a1d568836678
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2025-12-20 14:19:38 +0000

    build based on cae80fb

---
+++ versions.js
@@ -0,0 +1,17 @@
+var DOC_VERSIONS = [
+  "stable",
+  "v1.12",
+  "v1.11",
+  "v1.10",
+  "v1.9",
+  "v1.8",
+  "v1.7",
+  "v1.6",
+  "v1.5",
+  "v1.4",
+  "v1.3",
+  "v1.2",
+  "dev",
+];
+var DOCUMENTER_NEWEST = "v1.12.0";
+var DOCUMENTER_STABLE = "stable";
