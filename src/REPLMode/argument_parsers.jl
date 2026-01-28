37a2cbd0164564c8c494ff3d4652fb3f95e6f34b
Gamila <gamilawisam1710@gmail.com>
2026-01-28 17:35:08 +0200

    Fix indentation of @info in parse_package_identifier for clarity

--- src/REPLMode/argument_parsers.jl
+++ src/REPLMode/argument_parsers.jl
@@ -471,7 +471,7 @@ function parse_package_identifier(pkg_id::PackageIdentifier; add_or_develop = fa
                any(entry -> entry == name && isdir(joinpath(parent, entry)), readdir(parent))
                # only warn if it's a Julia package folder
                if isfile(joinpath(path, "Project.toml"))
-                @info "Use `./$word` to add or develop the local directory at `$(Base.contractuser(abspath(word)))`."
+                   @info "Use `./$word` to add or develop the local directory at `$(Base.contractuser(abspath(word)))`."
                end
             end
         end
